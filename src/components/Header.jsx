c40cd0ac6d8ea4a4bc53191a20377a4dbae83d74
Fernando Soares <fndcaique@gmail.com>
2021-10-05 15:18:22 -0300

    disponibilization test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,12 @@
 // implement Header component here
+import { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      'Header'
+    );
+  }
+}
+
+export default Header;
