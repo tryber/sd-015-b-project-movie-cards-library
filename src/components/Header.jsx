6be34de22bbb83767c4743be3df8296a62b91ce8
David Souza <dabcs@poli.ufrj.br>
2021-10-05 18:22:08 -0300

    Inicio do Projeto Requisitos 1-4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,14 @@
-// implement Header component here
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="movie-card-header">
+        <h1>Movie Cards Library</h1>
+        <span>📽️</span>
+      </header>
+    );
+  }
+}
+
+export default Header;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,10 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    return (<MovieCard />);
+  }
+}
+
+export default MovieList;
