e22a3c7bc92dba5cb92dd98fda88b4baea1cb9f9
Guilherme Saboia <gsaboia@yahoo.com>
2021-10-06 15:38:25 -0300

    Adds reqs 3 and 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props; // movies object
+    return (
+      <div className="movie-card">
+        <img src={ movie.imagePath } alt="movie" className="movie-card-image" />
+        
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
-// implement MovieList component here
+import PropTypes from 'prop-types';
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="movie-list">
+        { movies.map((movie, index) => <MovieCard key={ index } movie={ movie } />) }
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.isRequired,
+};
+
+export default MovieList;
