a370576eb256315fad1b671a40d9bad25ddd9f99
Camilla Del Guerso <camilladelguerso@gmail.com>
2021-10-05 22:26:15 -0300

    Adiciona o requisito 17 e a estilizacao

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,17 +1,23 @@
 // implement MovieList component here
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
-import MovieCard from "./MovieCard";
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const {movies} = this.props;
+    const { movies } = this.props;
     return (
-      <div>
-        {movies.map((elemento) => <MovieCard key={ elemento.title } movie={ elemento } />) }
+      <div className="movie-list">
+        {movies.map((element) => <MovieCard key={ element.title } movie={ element } />) }
       </div>
-    )
+    );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
 
 export default MovieList;
