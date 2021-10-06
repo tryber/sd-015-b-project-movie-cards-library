b75bcbd6ef71a259b1080bdf5745100f08f47e43
Rodrigo Lima <rodrigootavio19@gmail.com>
2021-10-06 16:12:21 -0400

    estiliza projeto

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,13 +10,15 @@ class MovieCard extends Component {
     const { title, subtitle, storyline, imagePath, rating } = movie;
 
     return (
-      <>
-        <img src={ imagePath } alt={ title } />
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
-        <Rating rating={ rating } />
-      </>
+      <div className="movie-card">
+        <img className="movie-card-image" src={ imagePath } alt={ title } />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+          <p className="movie-card-rating"><Rating rating={ rating } /></p>
+        </div>
+      </div>
     );
   }
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,7 +7,11 @@ class MovieList extends Component {
   render() {
     const { movies } = this.props;
 
-    return (movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />));
+    return (
+      <div className="movie-list">
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
+    );
   }
 }
 
