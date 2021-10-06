8436b8ba1f3002f17d9fa70ec7ebc370a463cae9
Alex Menezes <alexmenezes.business@outlook.com>
2021-10-06 19:24:53 -0300

    adiciona requisito 17 com correçao no lint

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 class Rating extends React.Component {
   render() {
     const { rating } = this.props;
-    console.log(rating);
     return (
       <h1 className="rating">
         {rating}
