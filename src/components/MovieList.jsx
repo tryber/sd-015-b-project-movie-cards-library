21b27a4090aaae5b7b9ef6489bee83254d6d707f
Flávio Bianchetti <fjb.developer@gmail.com>
2021-10-06 18:35:05 -0300

    Atendidos requisitos 4 e 6 - validados via npm

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,17 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return ({});
+    const { movies } = this.props;
+    return movies.map((movie, index) => <MovieCard key={ index } card={ movie } />);
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.object.isRequired,
+}.isRequired;
 
 export default MovieList;
