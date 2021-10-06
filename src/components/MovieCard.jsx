57d41f43e39801494492657a9ac33f333382f9c8
aaguero96 <andre.luiz_1996@hotmail.com>
2021-10-06 17:37:37 -0300

    Fix lint part 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends Component {
   render() {
-    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    const { movie } = this.props;
+    const { title, subtitle, storyline, rating, imagePath } = movie;
     return (
       <div>
-        <p>title: {title}</p>
-        <p>subtitle: {subtitle}</p>
-        <p>storyline: {storyline}</p>
-        <p>rating: {rating}</p>
-        <p>imagePath: {imagePath}</p>
+        <p>
+          title: {title}
+        </p>
+        <p>
+          subtitle: {subtitle}
+        </p>
+        <p>
+          storyline: {storyline}
+        </p>
+        <p>
+          rating: {rating}
+        </p>
+        <p>
+          imagePath: {imagePath}
+        </p>
       </div>
     );
   }
 }
+
+// MovieCard.propTypes = {
+//   movie: PropTypes.Object,
+// };
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
 
 class MovieList extends Component {
   render() {
-    const movies = this.props.movies;
+    const { movies } = this.props;
     return (
       movies.map((element, index) => (
         <section key={ index }>
           <MovieCard movie={ element } />
         </section>
       ))
-    )
+    );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.array,
+};
 
 export default MovieList;
