d0ee2a55d41ab0c9605af5d20b12b0d180992e9f
Gabriel Peralta <prates_peralta@hotmail.com>
2021-10-06 15:03:16 -0300

    inicia o projeto

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,29 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+
+    const allMovies = movies.map((film) => (
+      <MovieCard key={ film.title } movie={ film } />
+    ));
+
+    return <div className="movie-list">{ allMovies }</div>;
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
+
+export default MovieList;
