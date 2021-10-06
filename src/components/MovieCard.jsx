2c9c02935aea56a36ae47ce8d2dc14ad9eda0003
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2021-10-06 16:18:55 -0300

    Welckson Valentim - Finaliza requisito 3 apos aprovaçao local

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
 // implement MovieCard component here
+/* import React from 'react';
+import movies from './data';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+        <ul>{movies.map((movie) => movie)}</ul>
+    );
+  }
+}
+
+export default MovieCard;
+ */
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>test</div>
+    );
+  }
+}
+
+export default MovieList;
