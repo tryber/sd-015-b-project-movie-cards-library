88cb66bc95f22c3c1e6a4c227318f7c5632263ae
Virginia <vihvasc@gmail.com>
2021-10-06 18:37:22 -0300

    Bomba!

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
 // implement Rating component here
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div className="movie-card-rating">
+        <span className="rating">{ rating }</span>
+      </div>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
+
+export default Rating;
