b94f5edb2c14b170c962a723f80cd54afb1599f4
Rodrigo Lima <rodrigootavio19@gmail.com>
2021-10-06 13:56:58 -0400

    requisitos 3 e 4

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
 // implement MovieList component here
+import { Component } from 'react';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+
+    return ('oi');
+  }
+}
+
+export default MovieList;
