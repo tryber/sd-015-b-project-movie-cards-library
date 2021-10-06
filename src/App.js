872d3d3ebf9e3cef81ad5dd7300a7ee481b2aa6b
israel jeronimo <silvaij16@gmail.com>
2021-10-06 18:48:53 -0300

    1- Crie um componente `<Header />`

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
+      <Header />
       Começando o projeto!
     </div>
   );
