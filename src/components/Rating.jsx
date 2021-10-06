092b473721f0d397c3d1cee6e62b465f2b87a457
matheusg18 <matheusgabriel.18@gmail.com>
2021-10-06 17:30:12 -0300

    Requisito 12 feito

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,8 +1,15 @@
 // implement Rating component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class Rating extends React.Component {
   render() {
-    return null;
+    const { rating } = this.props;
+
+    return <span className="rating">{ rating }</span>;
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
