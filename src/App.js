dfc4757279b3a5788e4f393d562b9f721d2fa389
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2021-10-06 16:05:30 -0300

    finalmente comita 1 e 2 quesito com npm certo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import header from './components/Header';
+import Header from './components/Header';
 
 function App() {
   return (
-    <header />
+    <Header />
   );
 }
 
