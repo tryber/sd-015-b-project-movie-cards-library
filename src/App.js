ed0d47c8cab8b029337ed2477a38b85113d1b4dd
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-06 17:31:24 -0300

    Requisito 15, renderizando o Header no App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import MovieList from './components/MovieList';
 import movies from './data';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
+      <Header />
       <MovieList movies={ movies } />
     </div>
   );
