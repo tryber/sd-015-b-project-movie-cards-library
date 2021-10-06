05e36729b6ac7cd589581dd92317c9d6e65af0b5
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-06 16:46:55 -0300

    Requisito 6, recebendo e criando proptypes para as props do MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,30 @@
 // implement MovieCard component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props;
     return (
       <div>
         <h2>
-          Esse é um movie card
+          {title}
+          {subtitle}
+          {storyline}
+          {imagePath}
+          {rating}
         </h2>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  imagePath: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
 export default MovieCard;
