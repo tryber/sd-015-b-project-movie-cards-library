499a061e4a6e530401ca90db2d49801a6525e386
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2021-10-06 16:16:14 -0300

    adiciona todo projeto

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,31 @@
-// implement MovieCard component here
+import PropTypes from 'prop-types';
+import React from 'react';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, rating, imagePath } = movie;
+
+    return (
+      <div>
+        <img src={ imagePath } alt={ title } />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <Rating rating={ rating } />
+      </div>
+    );
+  }
+}
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+    storyline: PropTypes.string,
+    subtitle: PropTypes.string,
+    title: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,25 @@
-// implement MovieList component here
+import PropTypes from 'prop-types';
+import React from 'react';
+import MovieCard from './MovieCard';
+// import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      movies.map((movie, index) => (
+        <MovieCard
+          key={ movies[index].title }
+          movie={ movie }
+        />))
+    );
+  }
+}
+MovieList.propTypes = {
+  movies: PropTypes.shape({
+    map: PropTypes.func,
+  }).isRequired,
+};
+
+export default MovieList;
