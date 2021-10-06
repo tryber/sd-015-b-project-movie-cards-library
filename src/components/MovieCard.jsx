2cf712d863baecb38b3d9f9e8ab62f48d0758b14
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-10-06 16:17:08 -0300

    Requisito 4 com erros lint (props)

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,17 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    const { movies } = this.props;
+    const { title, subtitle } = movies;
+    return (
+      <article>
+        <p>{title}</p>
+        <p>{subtitle}</p>
+      </article>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
+    const { movies } = this.props;
     return (
-      <div>
-        Para o requisito 3
-      </div>
+      <section>
+        {movies.map((elem) => <MovieCard key={ elem.title } movie={ elem } />)}
+      </section>
     );
   }
 }
