8f3f4a40f54f801042560d462160da4543eaaeb3
Gabrielle-Murat <belle.murat@gmail.com>
2021-10-06 11:54:09 -0300

    Adiciona os requisitos de 6 a 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,34 @@
 // implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <article>
+        <h4>
+          {movie.title}
+        </h4>
+        <h5>
+          {movie.subtitle}
+        </h5>
+        <p>
+          {movie.storyline}
+        </p>
+        <img src={ movie.imagePath } alt={ movie.title } />
+      </article>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  })).isRequired,
+};
+
+export default MovieCard;
