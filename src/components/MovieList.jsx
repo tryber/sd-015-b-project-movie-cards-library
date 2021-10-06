726e50d19d6acb793c5222c0e665b35b2a9de509
Rodolpho Coller <rodolpho.coller12@gmail.com>
2021-10-06 14:54:26 -0300

    adiciona componemte MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <main>
+        movies
+      </main>
+    );
+  }
+}
+
+export default MovieList;
