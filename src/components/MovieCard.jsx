b0696aed110c40fce4941034ab31420c82f1d5b4
Denis Jonathan <denisjonathan7@gmail.com>
2021-10-06 13:19:08 -0300

    Requirement 7: completed

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,6 +3,16 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
+
+    return (
+      <div>
+        <div>
+          <img src={ imagePath } alt={ title } />
+        </div>
+      </div>
+    );
   }
 }
 
