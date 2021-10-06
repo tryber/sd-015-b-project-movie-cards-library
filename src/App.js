779e1178597821dce9d080d3308c8c95e6ceb986
Denis Jonathan <denisjonathan7@gmail.com>
2021-10-06 14:36:37 -0300

    Add footer to page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import Footer from './components/Footer';
 import movies from './data';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <MovieList movies={ movies } />
+      <Footer />
     </div>
   );
 }
