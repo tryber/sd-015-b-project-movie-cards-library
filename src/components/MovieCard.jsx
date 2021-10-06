763175081d54bceeb71e5b8e691e217849a80aa1
Yamamoto Andre <andre.yamamoto07@gmail.com>
2021-10-06 18:59:03 -0300

    Criaçao Prop Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 
 export default class MovieCard extends React.Component {
   render() {
-    const { movie } = this.props
+    const { movie } = this.props;
     return (
-    <section>
-              <h4>{movie.title}</h4>
-                <h5>{movie.subtitle}</h5>
-                <img src={movie.imagePath}></img>
-                <p>{movie.storyline}</p>
-            <Rating />
-         </section>
-        )
-    }
+      <section>
+        <h4>{movie.title}</h4>
+        <h5>{movie.subtitle}</h5>
+        <img src={movie.imagePath}></img>
+        <p>{movie.storyline}</p>
+        <Rating rating={movie.rating}/>
+      </section>
+    );
+  }
 }
