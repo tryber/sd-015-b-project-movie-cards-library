ef43bd73444a7bbd9cea381dfef94df5b867f244
DanMondaini <danielmondaini@outlook.com>
2021-10-06 16:16:22 -0300

    corrige 4 e 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,18 +1,21 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <div>
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-      </div>
+      movies.map((movie) => <MovieCard key={ movie.title } />)
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
 
 export default MovieList;
