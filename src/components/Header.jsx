b1c646661542e70288ae75a8a8fd5e83d2b12555
PedroAugmed <pedroaugmed7@gmail.com>
2021-10-06 18:27:50 -0300

    Startando projeto e ADD requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,12 @@
 // implement Header component here
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Hi i am a Header</h1>
+    </header>
+  );
+}
+
+export default Header;
