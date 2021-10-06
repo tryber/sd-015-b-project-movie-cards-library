9403557199c464e7beebb599a4f45214bbe6ee2c
David Souza <dabcs@poli.ufrj.br>
2021-10-06 16:05:53 -0300

    tentando resolver 4 e 5

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import React from 'react';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main className="movie-list">
-        <MovieList />
+        <MovieList movies={ movies } />
       </main>
     </div>
   );
