90cf1ee5ce6ded653ccdd2eb6ece578a4c9f8570
gustavogss <gustavogss.dev@gmail.com>
2021-10-06 15:06:14 -0300

    Corrigindo a versao do nmp para a 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,20 @@
-// implement MovieCard component here
+import React from 'react';
+import '../data';
+
+class MovieCard extends React.Component {
+  render() {
+    return (<h1>Card</h1>
+    //   <div className="movie-card movie-card-header">
+    //     <img className="movie-card-image" src={this.props.img} alt={ title } />
+    //     <h1 className="movie-card-title">{title}</h1>
+    //     <h2 className="movie-card-subtitle">{subtitle}</h2>
+    //     <p className="movie-card-storyline">{storyline}</p>
+    //     <hr />
+    //     <div className="rating">
+    //       <h3 className="movie-card-rating">{rating}</h3>
+    //     </div>
+    //   </div>
+    );
+  }
+}
+export default MovieCard;
