b5dd46d8fe52ce25eebea6a0b4a2a388a5a83ba5
Bruno Bartolomeu <bruno.bartolomeu5@hotmail.com>
2021-10-06 14:51:34 -0300

    Adiciona image nas cards dos filmes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
-// implement MovieCard component here
+import React from 'react';
+
+export default class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    console.log(movie);
+    return (
+      <div>
+        <img src={ movie.imagePath } alt={ movie.title }/>
+      </div>
+    );
+  }
+}
