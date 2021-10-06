eef5e985de098c05de24eb6bae6f4e0ec7723d45
iuryroque <iury.roque@hotmail.com>
2021-10-06 16:36:15 -0300

    Requisito 2 finalizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 // implement Header component here
 import React from 'react';
+import ReactPropTypes from 'prop-types';
 
 class Header extends React.Component {
   render() {
+    const {movies} = this.props;
     return (
-      <Header> </Header>
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
     );
   }
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
 // implement MovieList component here
+import React from "react";
+// import ReactPropTypes from 'prop-types';
+
+class MovieList extends React.Component{
+    render() {
+        // const { movies } = this.props;
+        return(
+
+        );
+    }
+}
+
+
+export default MovieList;
