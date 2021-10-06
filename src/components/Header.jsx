61337b618494227a606045b97d41bfe7770bc528
Flavio Junior <flaviopds.jr@gmail.com>
2021-10-06 17:38:21 -0300

    Corrige req 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div>
+      <header>
         <h1>
           Movie Cards Library
         </h1>
-      </div>
+      </header>
     );
   }
 }
