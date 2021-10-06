a852d188d7a7df87b84ee57b013266d46cb7cdc5
Flávio Bianchetti <fjb.developer@gmail.com>
2021-10-06 19:07:06 -0300

    Atendido requisito 8 - validado via npm

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,12 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    return <img src={ movie.imagePath } alt={ movie.title } />;
+    return (
+      <div>
+        <img src={ movie.imagePath } alt={ movie.title } />
+        <h4>{ movie.title }</h4>
+      </div>
+    );
   }
 }
 
