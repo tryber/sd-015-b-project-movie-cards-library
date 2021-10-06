6c5bea1f45e88b1697fbc8d8819870c09c070577
Caio R. Vieira <caio.robs98@gmail.com>
2021-10-06 15:04:25 -0300

    Component Rating pronto

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,10 @@
 // implement Rating component here
+import React from 'react';
+
+export default function Rating({ rating }) {
+  return (
+    <div>
+      <p className="rating">{rating}</p>
+    </div>
+  );
+}
