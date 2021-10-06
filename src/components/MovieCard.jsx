3bd3622dac4982c290f785ff8138dee0268c1cfa
Fernando Segregio <segregio@gmail.com>
2021-10-06 12:18:49 -0300

    Requisito 11, 12, 13, 14

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-//import movies from '../data';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, rating, imagePath } = movie;
 
     return (
       <section>
@@ -12,6 +12,7 @@ class MovieCard extends React.Component {
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
+        <Rating rating={rating} />
       </section>
 
     );
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 class Rating extends React.Component {
-    render(){
-
-    }
+  render() {
+    const { rating } = this.props;
+    return (
+      <div>
+        <p className="rating">{rating}</p>
+      </div>
+    );
+  }
 }
 
-export default Rating
+export default Rating;
