fe1ccdc519d162edd678b2bd76d680023c381b33
Flavio Junior <flaviopds.jr@gmail.com>
2021-10-06 16:03:07 -0300

    req 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 
 class MovieCard extends Component {
   render() {
+    const { movie } = this.props;
+    console.log(movie);
     return (
-      <h1>Ola</h1>
+      <section>
+        <img src={ movie.imagePath } alt={ movie.title } />
+      </section>
     );
   }
 }
