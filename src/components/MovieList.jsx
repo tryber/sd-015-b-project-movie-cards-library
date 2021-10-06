6517ae1d3c973a92fca0fd4ca76e0e286a7f94c5
Gabriel Borges <igblborges@gmail.com>
2021-10-06 15:54:07 -0300

    adicionando requisitos

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      movies.map((movie,
+        index) => <MovieCard key={ `Movie Title ${index + 1}` } movie={ movie } />)
+    );
+  }
+}
+export default MovieList;
