b082e1e10bfbc42a953dcd8c4bd0dd9cc2b7cfd7
Flávio Bianchetti <fjb.developer@gmail.com>
2021-10-06 20:20:53 -0300

    Atendido requisito 13 - validado via npm

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,7 @@
 // implement MovieCard component here
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class MovieCard extends React.Component {
         <h4>{ movie.title }</h4>
         <h5>{ movie.subtitle }</h5>
         <p>{ movie.storyline }</p>
+        <Rating />
       </div>
     );
   }
@@ -22,6 +24,7 @@ MovieCard.propTypes = {
     imagePath: PropTypes.string.isRequired,
     subtitle: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
   }).isRequired,
 };
 
