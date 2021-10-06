6233cc33d13dd8839703aa5bc9d85a244b5a7518
Deivid <deividmborges@gmail.com>
2021-10-06 18:16:48 -0300

    Corrige erros de lint e aplica proptype

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
 
 class MovieCard extends React.Component {
@@ -7,13 +8,13 @@ class MovieCard extends React.Component {
     return (
       <div>
         <div key={ movie.title } className="movie-card">
-          <img src={ movie.imagePath } alt="imagem do filme" className="movie-card-image" />
+          <img src={ movie.imagePath } alt="imagem filme" className="movie-card-image" />
           <div className="movie-card-body">
-            <h4 className="movie-card-title" >{ movie.title }</h4>
-            <h5 className="movie-card-subtitle" >{ movie.subtitle }</h5>
-            <p className="movie-card-storyline" >{ movie.storyline }</p>
+            <h4 className="movie-card-title">{ movie.title }</h4>
+            <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+            <p className="movie-card-storyline">{ movie.storyline }</p>
           </div>
-          <div className="movie-card-rating" >
+          <div className="movie-card-rating">
             <Rating rating={ movie.rating } />
           </div>
         </div>
@@ -22,6 +23,14 @@ class MovieCard extends React.Component {
   }
 }
 
-
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    imagePath: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
+};
 
 export default MovieCard;
