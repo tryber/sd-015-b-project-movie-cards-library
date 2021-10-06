9ffe2ece99c990e21dd31d4d3d6fe7541d05f599
DanMondaini <danielmondaini@outlook.com>
2021-10-06 15:49:22 -0300

    req 3 4 5 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,16 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Movie
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,19 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>{movies}</h2>
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+      </div>
+    );
+  }
+}
+
+export default MovieList;
