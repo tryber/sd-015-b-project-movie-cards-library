e26a29b02f5ab95164e3fb937a208d1345a9b230
douglas <douglasmonteiro.m@gmail.com>
2021-10-06 16:03:56 -0300

    Adiciona requisito 3 e 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,9 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (<p>ola mundo</p>);
+  }
+}
+
+export default MovieCard;
