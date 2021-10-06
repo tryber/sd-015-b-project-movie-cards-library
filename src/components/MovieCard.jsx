b4b239d6cd8174e11bd12221e560eb07048a5343
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-10-06 16:48:28 -0300

    Realiza requisitos 11, 12, 13, 14, 15 e 16

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     const { title, subtitle, storyline, rating, imagePath } = movie;
+    const rateNumber = rating;
     return (
       <div key={ subtitle }>
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
         <img src={ imagePath } alt={ `Imagem do filme ${title}` } />
-        <span>{rating}</span>
+        <Rating rating={ rateNumber } />
       </div>
     );
   }
