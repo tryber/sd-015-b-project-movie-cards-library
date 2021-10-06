e051d269adfea434f8209b2df218e13ebba9ab4d
Lucas Rodrigues <theluks21@live.com>
2021-10-06 18:35:53 -0300

    Adiciona requisito 3

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 // import MovieCard from './components/MovieCard';
-// import MovieList from './components/MovieList';
+import MovieList from './components/MovieList';
 // import Rating from './components/Rating';
+import Movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
-
+      <MovieList movies={ Movies } />
     </div>
   );
 }
