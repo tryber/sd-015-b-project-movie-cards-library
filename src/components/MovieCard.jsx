b14b5c2b0f8af2bfbce9c08b89e8920b4c877332
GabrielJorge <gabrieljorge.ca@gmail.com>
2021-10-06 14:26:39 -0400

    add MovieCard, MovieList, Header and Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,25 @@
-// implement MovieCard component here
+import React from 'react';
+import movies from '../data';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    return (movies.map((movie) => (
+      <div key={ movie.title } className="movie-card">
+        <img
+          className="movie-card-image"
+          src={ movie.imagePath }
+          alt={ movie.subtitle }
+        />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline">{ movie.storyline }</p>
+        </div>
+        <Rating movies={ movie.rating } />
+      </div>
+    )))
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div className="movie-list">
+        <MovieCard />
+      </div>
+    );
+  }
+}
+
+export default MovieList;
