83f5f7f502cd9316a35c8a1cb52e4ddef6566247
Rodrigo Lima <rodrigootavio19@gmail.com>
2021-10-06 13:58:24 -0400

    requisito 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,10 @@
 // implement MovieCard component here
+import { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    return ('oi');
+  }
+}
+
+export default MovieCard;
