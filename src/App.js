5daaf82604fbffd7bd362cb940d8d026212d03db
anniehau <anniehaurani@gmail.com>
2021-10-06 14:48:13 -0300

    Requisito 3 completo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList'
+import movies from './data.js'
+
+console.log(movies)
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={movies} />
     </div>
   );
 }
