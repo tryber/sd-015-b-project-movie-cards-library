667efb9dcd80aabe47e0b96788f6ad744de3ddf8
Marina Marques <marina.marqueso@outlook.com>
2021-10-06 16:25:38 -0300

    Adiciona desenvolvimento com map

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,24 @@
 // implement MovieCard component here
 import React from 'react';
-import movies from '../data';
+import movie from '../data';
 
 class MovieCard extends React.Component {
   render() {
-    return movies;
+    return (
+      movie.map((elemento) => (
+        <section key={ elemento.title }>
+          <h4>
+            { elemento.title }
+          </h4>
+          <h5>
+            { elemento.subtitle }
+          </h5>
+          <p>
+            {elemento.storyline}
+          </p>
+          <img src={ elemento.imagePath } alt={ elemento.title } />
+        </section>
+      )));
   }
 }
 
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,11 @@
 // implement MovieList component here
 import React from 'react';
-import movie from '../data';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     return (
-      <MovieCard movies={ movie.title } />
+      <MovieCard />
     );
   }
 }
