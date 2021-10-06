c067cac8f16241ff7488b7517025fc135d08001c
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-10-06 14:49:08 -0300

    Requisito 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div>
+      <header>
         <h1>Movie Cards Library</h1>
-      </div>
+      </header>
     );
   }
 }
