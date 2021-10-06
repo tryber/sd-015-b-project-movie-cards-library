d019c4a41a482c2b3259a2b7059094eedbb40c70
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-10-06 19:49:48 -0300

    Implementa MovieCard e Rating

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import MovieCard from './MovieCard';
+
+// const eachMovie = movies.map((movie) =
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <main>
-        <h1>
-          Filmes
-        </h1>
-      </main>
+      <div>
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
     );
   }
 }
