9e3bfd96d7e398cbfed337fb8d505db51ae2abf8
Thirki <cicero.santos62@etec.sp.gov.br>
2021-10-06 17:34:22 -0300

    feat: exibindo alguns cards em tela

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import data from './data';
 
 import './App.css';
 
@@ -7,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movie={ data } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,35 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <div>
+        {movie.map((element) => (
+          <MovieCard
+            movie={ element }
+            key={ element.title }
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movie: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
+
+export default MovieList;
