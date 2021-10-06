9fa884dc20654a12742b6efea0d9186d32edb501
Edson Lima <edsoneduardo526@gmail.com>
2021-10-06 17:22:10 -0300

    Adiciona movielist e movieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,33 @@
 // implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { film } = this.props;
+
+    return (
+      <div>
+        <img src={ film.imagePath } alt={ film.title } />
+        <h1>{ film.title }</h1>
+        <h2>{ film.subtitle }</h2>
+        <p>{ film.storyline }</p>
+        <p>{ film.rating }</p>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+
+  film: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,19 @@
 // implement MovieList component here
 import React from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
     return (
-      <section />
+      <div>
+        <section>
+          { movies.map((filme) => (<MovieCard
+            key={ filme.title }
+            film={ filme }
+          />))}
+        </section>
+      </div>
     );
   }
 }
