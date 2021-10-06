e437f1c7cc31d0da7f9e7debc4ead644e4488731
Lucas Rodrigues <theluks21@live.com>
2021-10-06 20:10:49 -0300

    Adiciona requisito 4

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,17 @@
 // implement MovieList component here
 import React from 'react';
+import MovieCard from './MovieCard';
+// import Movies from '../data';
 
 class MovieList extends React.Component {
   render() {
-    return <div />;
+    const movies = this.props.movies;
+
+    return (
+      <main>
+        {movies.map((el) => <MovieCard movie={el}/>)}
+      </main>
+    )
   }
 }
 
