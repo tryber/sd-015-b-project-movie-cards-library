cea2b4639b22eb2c2a7b2342fc634cdbf40be0ee
Arthur <viegas.arthur2013@hotmail.com>
2021-10-06 15:23:29 -0300

    Requisito 1: Fix lint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 class Header extends React.Component {
-  render () {
+  render() {
     return (
       <header>
         <h1>oi</h1>
       </header>
-    )
+    );
   }
 }
 
-export default Header
+export default Header;
