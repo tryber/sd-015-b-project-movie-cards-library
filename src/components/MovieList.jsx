248c83297060128adc9e726d335eb9d66406bbb5
Matheus <matheusfelippeh@gmail.com>
2021-10-06 15:55:33 -0300

    add MoveList Component

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,23 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        {
+          movies.forEach((movie) => (<MovieCard />))
+        }
+      </div>);
+  }
+}
+
+MovieList.PropTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline: PropTypes.string,
+  rating: PropTypes.number,
+  imagePath: PropTypes.string,
+};
+export default MovieList;
