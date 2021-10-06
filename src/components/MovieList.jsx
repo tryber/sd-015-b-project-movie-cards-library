185de298a10e03496cb21b7e35fc49c9a3059dda
aaguero96 <andre.luiz_1996@hotmail.com>
2021-10-06 16:34:46 -0300

    Finish part 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default MovieList;
