a9e342454981c06f35c2fd2d43a17cb21aae4b48
Rafael Colombo <rcolombodev@gmail.com>
2021-10-06 09:44:45 -0300

    Adiciona requisito 9

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,6 +9,7 @@ class MovieCard extends React.Component {
       <div className="movie-card">
         <img src={ imagePath } alt={ title } />
         <h4>{title}</h4>
+        <h5>{subtitle}</h5>
       </div>
     );
   }
