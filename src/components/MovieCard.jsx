a6596676a61c1e379f5eeb381495da47e3646ff4
Danielen Cestari Nunes <danielen.cestari@gmail.com>
2021-10-06 16:19:15 -0300

    requisito 08

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,10 @@ class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     return (
-      <img src={ movie.imagePath } alt={ movie.title } />
+      <>
+        <img src={ movie.imagePath } alt={ movie.title } />
+        <h4>{movie.title}</h4>
+      </>
     );
   }
 }
