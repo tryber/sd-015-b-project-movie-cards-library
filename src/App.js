d715e2b4fbbd4763a32f2fa48ee31e2f5d10cba9
Gustavo <fariaguga@hotmail.com>
2021-10-06 16:57:08 -0300

    Adiciona tudo (Commit do desespero)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
+import data from './data';
+import MovieList from './components/MovieList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      aa
+      <Header />
+      <MovieList movies={ data } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,5 +1,6 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
@@ -7,10 +8,27 @@ class MovieList extends React.Component {
     const { movies } = this.props;
     return (
       <div>
-        { movies.map((elemento) => <MovieCard />)}
+        { movies.map((movieElement) => (
+          <MovieCard
+            key={ movieElement.title }
+            movie={ movieElement }
+          />
+        )) }
       </div>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.number,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
 
 export default MovieList;
