3df1712bae4dc5b23fb21b86826fc961c5b68729
Yamamoto Andre <andre.yamamoto07@gmail.com>
2021-10-06 18:07:11 -0300

    criaçao do Header v1.0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
-      {}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+     
     </div>
   );
 }
