54a5898eb9a27d3585908e10bcb13ba5e359eb8e
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2021-10-06 17:44:33 -0300

    todos requesitos sem lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,33 @@
-// implement MovieCard component here
+/* eslint-disable react/require-default-props */
+import React from 'react';
+import propTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, rating, imagePath } = movie;
+
+    return (
+      <article>
+        <img src={ imagePath } alt={ title } />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <Rating rating={ rating } />
+      </article>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: propTypes.shape({
+    title: propTypes.string,
+    subtitle: propTypes.string,
+    storyline: propTypes.string,
+    rating: propTypes.number,
+    imagePath: propTypes.string,
+  }),
+};
+
+export default MovieCard;
