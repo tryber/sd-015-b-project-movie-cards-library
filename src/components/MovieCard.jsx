c57c22d32e0209672efc51ebeb3d741ba81dfdba
Caio R. Vieira <caio.robs98@gmail.com>
2021-10-06 15:03:45 -0300

    Component Movie Card pronto

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
 // implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+
+export default function MovieCard({ movie }) {
+  return (
+    <div>
+      <h4>{movie.title}</h4>
+      <h5>{movie.subtitle}</h5>
+      <p>{movie.storyline}</p>
+      <img src={ movie.imagePath } alt={ movie.title } />
+      <Rating rating={ movie.rating } />
+    </div>
+  );
+}
