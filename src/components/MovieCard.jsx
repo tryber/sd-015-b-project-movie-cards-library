7b463879117b6bbc5479791ad9a526f183a6aeab
Jesse Benevides <jessebenevides@gmail.com>
2021-10-06 15:52:27 -0300

    Adiciona requisito 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Este é o MovieCard</h3>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,13 @@
 // implement MovieList component here
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     return (
       <section>
         <h2>Este é o MovieList</h2>
+        <MovieCard />
       </section>
     );
   }
