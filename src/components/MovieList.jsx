d8fc5a9473e51d969307b7afa07147581909a5f5
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-10-06 15:01:24 -0300

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+  render() {
+    return (
+      <div>
+        Para o requisito 3
+      </div>
+    );
+  }
+}
+
+export default MovieList;
