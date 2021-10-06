d355e2cc5b06be29265cf16c5742ac835595ffc2
Mariah M. Rodrigues <mariah.manetti@gmail.com>
2021-10-06 15:58:05 -0300

    Requisito 1 - Criar componente <Header>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,14 @@
 // implement Header component here
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Movie Cards Library</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
