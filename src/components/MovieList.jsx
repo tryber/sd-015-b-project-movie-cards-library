fdb0daaa027e381b0d174b7921be3b2e27037399
Gustavo Gracio <gustavogracio@hotmail.com>
2021-10-06 15:30:36 -0300

    Finaliza requisitos referentes ao Cards de filmes

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,26 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCards from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      movies.map((movie) => (
+        <MovieCards key={ movie.title } movie={ movie } />
+      ))
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })).isRequired,
+};
+
+export default MovieList;
