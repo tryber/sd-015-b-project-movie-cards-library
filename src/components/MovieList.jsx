8c101abf8ab0c205c6d90c2cc3c929f788fd1535
Marina Marques <marina.marqueso@outlook.com>
2021-10-06 15:14:21 -0300

    Conserta erros NPM e LINT

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,10 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return <h1>oi</h1>;
+  }
+}
+
+export default MovieList;
