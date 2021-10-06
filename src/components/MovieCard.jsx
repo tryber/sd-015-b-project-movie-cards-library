7c8def37473c8c29b3daafe1928d78da00b555e0
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-10-06 16:17:11 -0300

    Realiza requisitos 3 e 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,42 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, rating, imagePath } = movie;
+    return (
+      <div>
+        <h3>{title}</h3>
+        <h4>{subtitle}</h4>
+        <p>{storyline}</p>
+        <img src={ imagePath } alt={ `Imagem do filme ${title}` } />
+        <span>{rating}</span>
+      </div>
+    );
+  }
+}
+
+const notFind = 'Não encontrado';
+
+MovieCard.defaultProps = {
+  movie: {
+    title: notFind,
+    subtitle: notFind,
+    storyline: notFind,
+    imagePath: notFind,
+    rating: NaN,
+  },
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  }),
+};
+
+export default MovieCard;
