c470bf0dcac82727cf6c398f69c0261415a66cd1
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-06 15:29:03 -0300

    Criando e usando movielist

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>{movies}</h2>
+      </div>
+    );
+  }
+}
+
+export default MovieList;
