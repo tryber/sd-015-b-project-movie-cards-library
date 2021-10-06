db41c9ea8488fabaf66d5ca3a918ebff0d7aba4b
BrenoRCBrito <brenorcbrito@gmail.com>
2021-10-06 18:00:32 -0300

    corrige erro de css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,17 +7,13 @@ class MovieCard extends React.Component {
     const { movie } = this.props;
     return (
       <section className="movie-card">
-        <div>
-          <img className="movie-card-image" src={ movie.imagePath } alt={ movie.title } />
-        </div>
+        <img className="movie-card-image" src={ movie.imagePath } alt={ movie.title } />
         <div className="movie-card-body">
           <h4 className="movie-card-title">{ movie.title }</h4>
           <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
           <p className="movie-card-storyline">{ movie.storyline }</p>
         </div>
-        <div className="movie-card-rating">
-          <Rating rating={ movie.rating } />
-        </div>
+        <Rating rating={ movie.rating } />
       </section>
     );
   }
