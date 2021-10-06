7be8cda00635bd2f3a3aed1f4eb15e13e9391897
Antônio Campos <aclcampos@live.com>
2021-10-06 20:13:35 -0300

    Realizado requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
 // implement MovieList component here
+
+import React from 'react';
+import movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return movies;
+  }
+}
+
+export default MovieList;
