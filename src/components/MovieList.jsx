13511e785736136220f697f744376c3624c5e24a
Yamamoto Andre <andre.yamamoto07@gmail.com>
2021-10-06 18:41:22 -0300

    MovieList lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,18 +1,17 @@
 // implement MovieList component here
 import React from 'react';
-import movies from '../data'
-
+import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
     return (
       <section>
         {movies.map((movie) => (
-            <MovieCard movies={movie}/>
-            ))}
-        </section>
+          <MovieCard movies={ movie } />
+        ))}
+      </section>
     );
-    }
+  }
 }
 
 export default MovieList;
