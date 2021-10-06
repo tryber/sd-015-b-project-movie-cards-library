72392e3b2fcaa6f76ede553a9625383eb5d062d5
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-10-06 16:07:19 -0300

    Estilizado pagina

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList />
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      <MovieList movies={ movies } />
+      <footer>Criado por: Gabriel Benedicto</footer>
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,38 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-
-const movies = [
-  {
-    title: 'Movie Title 1',
-    subtitle: 'Movie Subtitle 1',
-    storyline: 'Movie Storyline 1',
-    rating: 4.5,
-    imagePath: 'images/movie_1',
-  },
-  {
-    title: 'Movie Title 2',
-    subtitle: 'Movie Subtitle 2',
-    storyline: 'Movie Storyline 2',
-    rating: 4.5,
-    imagePath: 'images/movie_2',
-  },
-  {
-    title: 'Movie Title 3',
-    subtitle: 'Movie Subtitle 3',
-    storyline: 'Movie Storyline 3',
-    rating: 3,
-    imagePath: 'images/movie_3',
-  },
-];
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <section>
+      <section className="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </section>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+};
+
+MovieList.defaultProps = {
+  movies: [],
+};
 
 export default MovieList;
