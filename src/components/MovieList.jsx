fe783eb0d17683f082c72ed8d7439e19b9994e4b
Antonio Souza <antoniofelipe2001@hotmail.com>
2021-10-06 15:03:43 -0300

    Requisito 3 finalizado

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+// import movies from '../data'
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <li>{movies}</li>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf.isRequired,
+};
+
+export default MovieList;
