b832f3e7435e3d171217b7a3b1f78ccf31c53f2e
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-10-06 14:53:22 -0300

    Requisito 3 completo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header.jsx';
+import MovieList from '../components/MovieList.jsx';
+import movies from './data.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={movies} />
     </div>
   );
 }
