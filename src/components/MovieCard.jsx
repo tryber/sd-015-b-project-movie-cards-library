0457e52a2fca1c6508b6f4fd84df683023d85487
Matheus <matheusfelippeh@gmail.com>
2021-10-06 17:35:04 -0300

    add classes for styles

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,12 +6,14 @@ class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     return (
-      <div>
-        <img src={ movie.imagePath } alt="Banner" />
-        <h4>{ movie.title }</h4>
-        <h5>{ movie.subtitle }</h5>
-        <p>{ movie.storyline }</p>
-        <Rating rating={ movie.rating } />
+      <div className="movie-card">
+        <img src={ movie.imagePath } alt="Banner" className="movie-card-image" />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline">{ movie.storyline }</p>
+          <Rating rating={ movie.rating } className="movie-card-rating" />
+        </div>
       </div>
     );
   }
