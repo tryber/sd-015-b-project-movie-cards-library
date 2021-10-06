5ddd8cf187fb1c11526b871b79d1db5eb21b1b42
Murilo <murilocarrijoadm@gmail.com>
2021-10-06 18:12:11 -0300

    Adiciona o 3º e o 4º requisito

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
-// implement MovieCard component here
+import React from "react";
+
+class MovieCard extends React.Component {
+  render() {
+    const {movie} = this.props;
+
+    return (
+      <h2>Componete MovieCard</h2>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+// import PropTypes from 'prop-types';
+import React from "react";
+
+class MovieList extends React.Component {
+  render() {
+    
+    const {movie} = this.props;
+
+    return (
+      <h2>Criando componente MovieList</h2>
+    );
+  }
+}
+
+export default MovieList;
