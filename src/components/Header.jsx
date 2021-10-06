fb8201ccc805c5376d6f93a9ffa180dee56da388
José Erivam <erivamdev@gmail.com>
2021-10-06 15:15:06 -0300

    adding requisite one

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-// implement Header component here
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movies</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
