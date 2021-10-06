42feacd0618ba3a596ad1def42461690f2948426
Joao Marco <joaomarco.matta@gmail.com>
2021-10-06 11:02:28 -0300

    Implementa requisitos 3, 4 e 5.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+import data from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    return (
+      <div>
+        {data.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
