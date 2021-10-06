78c05974875b231cc3dd6d96913e189e961e8702
Rafael Colombo <rcolombodev@gmail.com>
2021-10-06 10:46:55 -0300

    Adiciona requisito 17

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
       <section>
-        <p className="rating">{rating}</p>
+        <p className="rating">{ rating }</p>
       </section>
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.arrayOf(
+    PropTypes.number,
+  ).isRequired,
+};
 
 export default Rating;
