9e378ba409dc5c46e12be88ac987d3983bc499a7
1lucascq <1lucascq@gmail.com>
2021-10-06 11:51:55 -0300

    Adiciona validaçoes das props do MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,22 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Rating from './Rating';
 
-class MovieCard extends React.Component {  
+class MovieCard extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      this.props.movies.map((movie, i) => (
+      movies.map((movie, i) => (
         <div className="movie-card" key={ i }>
-          <img className="movie-card-image" src={movie.imagePath} alt={ `${movie.title} pic` } />
-          <h4 className="movie-card-title">{movie.title}</h4>
-          <h5 className="movie-card-subtitle">{movie.subtitle}</h5>
-          <p className="movie-card-storyline">{movie.storyline}</p>
+          <img
+            className="movie-card-image"
+            src={ movie.imagePath }
+            alt={ `${movie.title} pic` }
+          />
+          <h4 className="movie-card-title">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline">{ movie.storyline }</p>
           <div className="movie-card-rating">
-            <Rating rating={movie.rating} />
+            <Rating rating={ movie.rating } />
           </div>
         </div>
       ))
     );
   }
 }
+
+MovieCard.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieCard;
