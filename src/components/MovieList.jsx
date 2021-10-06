47e40a500dbced13e15cecf0cb52dc870b3b0d64
Gabriel Borges <igblborges@gmail.com>
2021-10-06 17:55:14 -0300

    test

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import MovieCard from './MovieCard';
-import movies from '../data';
+
+const movies = [
+  {
+    title: 'Movie Title 1',
+    subtitle: 'Movie Subtitle 1',
+    storyline: 'Movie Storyline 1',
+    rating: 4.5,
+    imagePath: 'images/movie_1',
+  },
+  {
+    title: 'Movie Title 2',
+    subtitle: 'Movie Subtitle 2',
+    storyline: 'Movie Storyline 2',
+    rating: 4.5,
+    imagePath: 'images/movie_2',
+  },
+  {
+    title: 'Movie Title 3',
+    subtitle: 'Movie Subtitle 3',
+    storyline: 'Movie Storyline 3',
+    ratingThree: 3,
+    imagePath: 'images/movie_3',
+  },
+];
 
 class MovieList extends React.Component {
   render() {
     return (
-      movies.map((movie,
-        index) => <MovieCard key={ `Movie Title ${index + 1}` } movie={ movie } />)
+      movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
     );
   }
 }
