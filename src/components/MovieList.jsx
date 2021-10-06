7238a2e693f80dff37efc01eabd25b0aa6917ec1
Danielen Cestari Nunes <danielen.cestari@gmail.com>
2021-10-06 15:25:17 -0300

    requisito 03

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const {movies}=this.props;
+    return (
+      <h1>teste</h1>
+    );
+  }
+}
+
+export default MovieList;
