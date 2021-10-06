02b64620339be413b8e43a2d47abf0f36521ca57
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-10-06 16:18:51 -0300

    Requisito 12: Renderiza a nota de um filme dentro de Rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 
 class Rating extends Component {
   render() {
+    const { rating } = this.props;
+
     return (
       <div>
-        teste
+        <p className="rating">{ rating }</p>
       </div>
     );
   }
