a264032cbd0001f7ec9ae0374211037e49bf65c0
Paulo Flóra <tflora.2004@gmail.com>
2021-10-06 15:01:21 -0300

    Feito o requisito 3.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div>
+        <p> Olá </p>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
