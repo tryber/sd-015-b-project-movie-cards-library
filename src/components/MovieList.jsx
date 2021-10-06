21aaa79a1b4f18bb505887b01df50f0c7d97c5d0
Jesse Benevides <jessebenevides@gmail.com>
2021-10-06 15:29:35 -0300

    Adiciona requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section>
+        <h2>Este é o MovieList</h2>
+      </section>
+    );
+  }
+}
+
+export default MovieList;
