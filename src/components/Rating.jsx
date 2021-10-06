d7eaa7167ec706cbc3d94f30c8c94147ff424d05
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-10-06 18:20:29 -0300

    Finaliza projeto

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -6,7 +6,7 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <p>{ rating }</p>
+      <p className="rating">{ rating }</p>
     );
   }
 }
