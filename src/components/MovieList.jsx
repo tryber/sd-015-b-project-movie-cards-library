8feb843571f16ad236529db485dc8c48e065d8f2
Hugoacr <hugoacr@yahoo.com.br>
2021-10-06 15:36:26 -0300

    cria componente MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div className="movie-list">
+        <MovieCard movie={ movies } />
+      </div>
+    );
+  }
+}
+
+export default MovieList;
