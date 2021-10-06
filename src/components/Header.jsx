8a2da439a9398e11f93cf97ed4810bf22e2ec8df
Joao Pedro de Oli. Lage <JoaoPedro.Lage@outlook.com>
2021-10-06 17:55:20 -0300

    Cria e Renderiza o <Header />

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,15 @@
-// implement Header component here
+import React from 'react';
+// import { v4 as generateId } from 'uuid';
+// import PropTypes  from 'prot-types';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
