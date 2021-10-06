1895292ca01aa529a9a6ad399870fe963d5c8752
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-10-06 15:15:35 -0300

    Requisito 14

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,14 +5,14 @@ import Rating from './Rating';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div>
         <img src={ imagePath } alt={ title } />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
-        <Rating />
+        <Rating rating={ rating } />
       </div>
     );
   }
@@ -24,6 +24,7 @@ MovieCard.propTypes = {
     subtitle: PropTypes.string.isRequired,
     imagePath: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
   }).isRequired,
 };
 
