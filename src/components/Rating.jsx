39fd96e64756a46c47d9e547e284c8f8644f1b4a
anniehau <anniehaurani@gmail.com>
2021-10-06 15:41:37 -0300

    Requisito 13 e 14 completos com lint tambem arrumado

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,10 +3,15 @@ import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
     return (
-      <h1 />
-    )
+      <p>{rating}</p>
+    );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;
