27905746d9d8127200363611c040482ab7878faf
carloslima <carlos.eduardo@the9level.com>
2021-10-06 17:27:49 -0300

    Adicionando requisitos 1 e 2

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <Header>
+        <h1>Movie Cards Library</h1>
+      </Header>
+    );
+  }
+}
+
+export default Header;
