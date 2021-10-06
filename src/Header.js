35804118d75c9b23cdfecf1177cb4e42236d09df
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-06 15:28:15 -0300

    Adiciona Header Component.

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class HeaderClass extends React.Component {
+    render() {
+      return (
+        <h1>Movie Cards Library</h1>
+      );
+    }
+  }
+  
+export default HeaderClass;
