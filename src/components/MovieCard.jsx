2617493815e9af9af3b585128c67c5169c5b61d8
Marco <marquinco@outlook.com>
2021-10-06 18:30:22 -0300

    testa funcionamento do avaliador

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,30 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { atual, key } = this.props;
+    console.log(key);
+    return (
+      <article className="movie-card">
+        <img src={ atual.imagePath } alt={ atual.title } />
+        <h3>{ atual.title }</h3>
+        <h4>{ atual.subtitle }</h4>
+        <p>{ atual.storyline }</p>
+        <p>{ atual.rating }</p>
+      </article>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  atual: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+  key: PropTypes.number.isRequired,
+};
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <main className="movie-list">
+        {movies.map((indivi, index) => <MovieCard key={ index } atual={ indivi } />)}
+      </main>
+    );
+  }
+}
+
+export default MovieList;
