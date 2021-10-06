4fb3d86f5bd5f974ebec85deb94ba6583366b07f
Caio R. Vieira <caio.robs98@gmail.com>
2021-10-06 15:32:22 -0300

    propTypes of MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,39 @@
 // implement MovieCard component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
 
-export default function MovieCard({ movie }) {
+// Greeting.propTypes = {
+//   name: PropTypes.string,
+//   lastName: PropTypes.string,
+// };
+
+function MovieCard({ movie, key }) {
+  console.log(key);
   return (
-    <div>
-      <h4>{movie.title}</h4>
-      <h5>{movie.subtitle}</h5>
-      <p>{movie.storyline}</p>
-      <img src={ movie.imagePath } alt={ movie.title } />
+    <div className="movie-card movie-card-body">
+      <h4 className="movie-card-title">{movie.title}</h4>
+      <h5 className="movie-card-subtitle">{movie.subtitle}</h5>
+      <p className="movie-card-storyline">{movie.storyline}</p>
+      <img src={ movie.imagePath } alt={ movie.title } className="movie-card-image" />
       <Rating rating={ movie.rating } />
     </div>
   );
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+  key: PropTypes.string,
+};
+
+MovieCard.defaultProps = {
+  key: 'MovieTitle',
+};
+
+export default MovieCard;
