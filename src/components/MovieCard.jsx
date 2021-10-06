df65887053c3c585866d0a77ee3c97b286df5457
Rodolfo <rodolfoserralha@hotmail.com>
2021-10-06 17:27:09 -0300

    tentando resolver css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 // implement MovieCard component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
 
 class MovieCard extends React.Component {
@@ -9,12 +10,22 @@ class MovieCard extends React.Component {
       <section>
         <img src={ movie.imagePath } alt={ movie.title } />
         <h4>{ movie.title }</h4>
-        <h5>{ movie.subtitule }</h5>
+        <h5>{ movie.subtitle }</h5>
         <p>{ movie.storyline }</p>
         <Rating rating={ movie.rating } />
       </section>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
