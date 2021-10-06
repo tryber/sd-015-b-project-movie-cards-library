c924f43b7f2f20fba23bcc9f9bd85865072dd2ef
Marina Marques <marina.marqueso@outlook.com>
2021-10-06 15:56:08 -0300

    Desenvolve MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
 // implement MovieList component here
+import React from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+  <MovieCard movie={movies.title} />
+  }
+}
+
+export default MovieList;
