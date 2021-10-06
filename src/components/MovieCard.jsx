2c736d5206b50b6357fe517725262034bfc48975
Guilherme Saboia <gsaboia@yahoo.com>
2021-10-06 16:56:51 -0300

    Adds final style

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,9 +8,11 @@ class MovieCard extends React.Component {
     return (
       <div className="movie-card">
         <img src={ movie.imagePath } alt="movie" className="movie-card-image" />
-        <h4>{ movie.title }</h4>
-        <h5>{ movie.subtitle }</h5>
-        <p>{ movie.storyline }</p>
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline">{ movie.storyline }</p>
+        </div>
         <Rating rating={ movie.rating } />
       </div>
     );
