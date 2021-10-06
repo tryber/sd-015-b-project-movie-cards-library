3e945027d873b4b4cc068e14ddee1e1a6f2b0e4f
anniehau <anniehaurani@gmail.com>
2021-10-06 15:15:49 -0300

    Lint arrumado para Requisitos 3-5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    const movie = this.props.movie;
+    const { movie } = this.props;
     return (
       <div>
         <h1>{movie.title}</h1>
         <h2>{movie.subtitle}</h2>
         <p>{movie.storyline}</p>
-        <p>Rating: {movie.rating}</p>
+        <p>
+          Rating:
+          {movie.rating}
+        </p>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const movies = this.props.movies;
+    const { movies } = this.props;
     return (
       <main>
-        {movies.map((movie) => <MovieCard movie={movie} key={movie.title} />)}
+        {movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />)}
       </main>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.isRequired,
+};
 
 export default MovieList;
