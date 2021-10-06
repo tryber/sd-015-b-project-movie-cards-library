e8d2919c7ce6580351fe37310e3c1814b0a85f4f
Wilian Pereira <wilian_pereira92@outlook.com>
2021-10-06 17:43:21 -0300

    Adicionado Requisito 4, 5 e 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>teste</h2>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
