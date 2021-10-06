ce2d9c12eb0f2ab434a8a09832b78bf6bb9c5cd2
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-06 15:41:09 -0300

    Criando e jogando MovieCard dentro do MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,14 @@
 // implement MovieList component here
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     return (
       <div>
-        <h2>{movies}</h2>
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
       </div>
     );
   }
