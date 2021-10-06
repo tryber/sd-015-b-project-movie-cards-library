04c0e10cab158a31940538eeb97cbdcc1ba66f04
Murilo <murilocarrijoadm@gmail.com>
2021-10-06 16:43:54 -0300

    Refatora o 1º requisito e Adiciona o 2º requisito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
+    return (
+      <header className="movie-card-header">Movie Cards Library</header>
+    );
   }
 }
 
