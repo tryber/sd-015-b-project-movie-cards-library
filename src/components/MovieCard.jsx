637251c9d4047214e0f22dfe21654a11e9d34d8b
Danielen Cestari Nunes <danielen.cestari@gmail.com>
2021-10-06 16:21:46 -0300

    requisitos 09 e 10 pq são gêmeos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,6 +8,8 @@ class MovieCard extends React.Component {
       <>
         <img src={ movie.imagePath } alt={ movie.title } />
         <h4>{movie.title}</h4>
+        <h5>{movie.subtitle}</h5>
+        <p>{movie.storyline}</p>
       </>
     );
   }
