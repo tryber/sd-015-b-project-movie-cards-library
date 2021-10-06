606088f5dbe610f729e158dbd5e9ffc3452a937f
Nicolas Franzolin <nicolas.franzolin@gmail.com>
2021-10-06 15:00:17 -0300

    Adiciona Requisito 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,12 @@ import React from 'react';
 
 class Header extends Component {
   render() {
-    return <header></header>;
-}
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
 }
 
 export default Header;
