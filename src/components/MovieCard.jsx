80b7b4b0f50fcf2f3bdb2bf008e0b486f56b07a0
matheusg18 <matheusgabriel.18@gmail.com>
2021-10-06 17:33:55 -0300

    Requisito 14 feito

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,14 +6,14 @@ import Rating from './Rating';
 export default class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div className="movie-card">
         <img src={ imagePath } alt={ `${title} movie` } />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
-        <Rating />
+        <Rating rating={ rating } />
       </div>
     );
   }
@@ -25,5 +25,6 @@ MovieCard.propTypes = {
     subtitle: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
     imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
   }).isRequired,
 };
