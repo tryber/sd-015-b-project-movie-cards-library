e2930f2514f66923deacea1ff1d111678f9c00e7
Rafael Colombo <rcolombodev@gmail.com>
2021-10-06 10:28:28 -0300

    Adiciona requisito 12, 13 e 14

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
 
     return (
       <div className="movie-card">
         <img src={ imagePath } alt={ title } />
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
       </div>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,13 @@
-import React from "react";
-
+import React from 'react';
 
 class Rating extends React.Component {
   render() {
-    
+    const { rating } = this.props;
+    return (
+      <section>
+        <p className="rating">{rating}</p>
+      </section>
+    );
   }
 }
 
