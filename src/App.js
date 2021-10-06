6794ec1860d30366886f9aa322593d67f1e92f5b
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-06 20:05:23 -0300

    Adiciona import do MovieCard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieCard from './components/MovieCard';
 
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
       <Header />
+      <MovieCard />
     </div>
   );
 }
