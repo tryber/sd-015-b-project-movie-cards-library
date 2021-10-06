9bd69f0c857da33af5a8abde86c4f74cda8b68df
Danilo <dcalado.fisio@yahoo.com.br>
2021-10-06 15:22:42 -0300

    requesito 5?

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import Rating from './components/Rating';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
       <Header />
       <MovieList />
+      <Rating />
       Hora de codar! 🚀✍️👨‍💻👩‍💻💪
     </div>
   );
