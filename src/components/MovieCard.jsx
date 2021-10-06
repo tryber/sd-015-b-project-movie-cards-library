16c8be9d6b1c310d2bc65d0abf77669ff326583d
Danilo <dcalado.fisio@yahoo.com.br>
2021-10-06 15:14:51 -0300

    requesito 3 e 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+
+    return (
+      <section>{movie}</section>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.arrayOf.isRequired,
+};
+
+export default MovieCard;
