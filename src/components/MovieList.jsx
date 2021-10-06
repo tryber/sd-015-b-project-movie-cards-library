4f1e1244323b7d53ce8544cdb18fde7b2d2036e2
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-10-06 15:32:14 -0300

    Renderiza MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+    // {movies.map(movie => <MovieCard movie={movie} /> )}
+      <h1>Ola</h1>
+    );
+  }
+}
+
+export default MovieList;
