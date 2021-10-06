8643f6f2422ff9f17b054a24625a79c1cf857eef
Marco <marquinco@outlook.com>
2021-10-06 18:54:27 -0300

    conserta codigo para cumprir requisitos 4,5,6

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,22 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <main className="movie-list">
-        {movies.map((indivi, index) => <MovieCard key={ index } atual={ indivi } />)}
+        {movies.map((ind) => <MovieCard key={ ind.title } atual={ ind } />)}
       </main>
     );
   }
 }
 
 export default MovieList;
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
