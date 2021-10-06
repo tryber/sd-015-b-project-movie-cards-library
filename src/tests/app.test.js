07b8d94550e175b789a3f7e83841a21e8e73a447
aaguero96 <andre.luiz_1996@hotmail.com>
2021-10-06 19:38:33 -0300

    Fix test

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import App from '../App';
+import App from '../components/App';
 
 describe('15 - Crie um componente `<App />`', () => {
   let wrapper;
