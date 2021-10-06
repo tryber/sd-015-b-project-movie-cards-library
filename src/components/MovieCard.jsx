a9b891d5fc6101c8054b9c9e813e6a729de631d2
davi <davisoarescoura@gmail.com>
2021-10-06 20:59:58 +0200

    adiciona requisitos do 3 ao 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,21 @@
 // implement MovieCard component here
+import React from 'react';
+// import PropTypes from 'prop-tipes';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <article>
+        <img src={ movie.imagePath } alt={ movie.title } />
+        <h4>{ movie.title }</h4>
+        <h5>{ movie.subtitle }</h5>
+        <p>{ movie.storyline }</p>
+
+
+      </article>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
 // implement MovieList component here
+import React from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section>
+        {movies.map((movie) => {
+          return <MovieCard key={ movie.title } movie={ movie } />;
+        })}
+      </section>
+    );
+  }
+}
+
+export default MovieList;
