18bb19febc00c3efb4e15feac12336da6ded15d8
Flávio Bianchetti <fjb.developer@gmail.com>
2021-10-06 20:58:50 -0300

    Incluidos elementos e classes para estilizaçao - Os requisitos do projeto nao foram modificados

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,12 +6,16 @@ import MovieCard from './MovieCard';
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-    return movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />);
+    return (
+      <div className="movie-list">
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+      </div>
+    );
   }
 }
 
 MovieList.propTypes = {
-  movies: PropTypes.object.isRequired,
+  movies: PropTypes.object,
 }.isRequired;
 
 export default MovieList;
