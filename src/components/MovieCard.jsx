73260610846e0486b17febab3b9bff6875d6a921
Arthur <viegas.arthur2013@hotmail.com>
2021-10-06 17:07:31 -0300

    Requirement 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movies } = this.props;
+    const { title, imagePath } = movies;
     return (
-      <h1>teste</h1>
+      <div>
+        <img src={ imagePath } alt={ title } />
+      </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movies: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../App.css';
 import movies from '../data';
 import MovieCard from './MovieCard';
 
