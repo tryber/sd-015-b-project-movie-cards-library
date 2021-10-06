d11e74527112661327cec5b3685a09e39017e340
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-06 03:58:16 -0300

    Faz checagem de tipos com PropTypes no arquivo Rating.jsx

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,5 +1,6 @@
 // implement Rating component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
@@ -9,5 +10,9 @@ class Rating extends React.Component {
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;
