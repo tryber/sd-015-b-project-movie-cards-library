3417a93f839160538fc2c2c790383ec7a766b177
Walace <walacewab@gmail.com>
2021-10-06 18:16:57 -0300

    Adiciona requisito do 4 a 10

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,32 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return <div>lista</div>;
+    const { movies } = this.props;
+    return (
+      <div>
+        {
+          movies.map((movie) => (<MovieCard key={ movie.title } movie={ movie } />))
+        }
+      </div>);
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
