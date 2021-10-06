3f95f9c857f5bd3f47472071a36430db6b3f1a85
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-06 17:22:06 -0300

    Requisito 11, criando componente Rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,16 @@
 // implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          teste
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Rating;
