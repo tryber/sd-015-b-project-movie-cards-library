33a0937fed78506b160ebb4cec8d12cb2e72f380
Rafael Colombo <rcolombodev@gmail.com>
2021-10-06 10:10:11 -0300

    Adiciona requisito 1,2,3,4 e 5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath, rating } = movie;
+    const { title, subtitle, storyline, imagePath } = movie;
 
     return (
       <div className="movie-card">
