52524abdc8a73fa5ff98cb95454365e61a1d63ff
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-10-06 20:12:48 -0300

    Cumprido o requisito 11

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
+    const { rating } = movie;
     return (
       <article>
         <div>
           <img src={ movie.imagePath } alt={ `Imagem do cartaz de ${movie.subtitle}` } />
           <h4>{movie.title}</h4>
-          <p>{movie.subtitle}</p>
+          <h5>{movie.subtitle}</h5>
           <p>{movie.storyline}</p>
         </div>
-        <div>
-          <h3>Rating</h3>
-          <p>{movie.rating}</p>
-        </div>
+        <Rating rating={ rating } />
       </article>
     );
   }
