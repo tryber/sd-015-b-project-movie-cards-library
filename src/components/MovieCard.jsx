c3d983cd754bfdb4f589097a3fdaa04423f68d46
Nicolas Franzolin <nicolas.franzolin@gmail.com>
2021-10-06 17:23:06 -0300

    Adiciona Requisitos 11, 12, 13, 14

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 // implement MovieCard component here
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -10,9 +11,12 @@ class MovieCard extends React.Component {
       <div>
         <div>
           <img src={ imagePath } alt={ title } />
-          <h4>{ title }</h4>
-          <h5>{ subtitle }</h5>
-          <p>{ storyline }</p>
+          <h4>{title}</h4>
+          <h5>{subtitle}</h5>
+          <p>{storyline}</p>
+        </div>
+        <div>
+          <Rating rating={ rating } />
         </div>
       </div>
     );
