f77effa1a270b56bdec617a2d0d0191f677d5d77
Allan Melo <allan-m-silva@hotmail.com>
2021-10-06 15:59:05 -0300

    adiciona requisito 4

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const movies = this.props;
     return (
       <section>
-        {movies}
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
       </section>
     );
   }
