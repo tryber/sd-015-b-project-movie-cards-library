5ea705849d607b697d2cae62fcdb51c5efe40965
Yamamoto Andre <andre.yamamoto07@gmail.com>
2021-10-06 19:23:18 -0300

    PropType Rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,11 +1,16 @@
 // implement Rating component here
+import PropTypes from 'prop-types';
 import React from 'react';
 
-export default class Rating extends React.Component{
-    render() {
-        const {rating}=this.props
-        return(
-            <span className='rating'>{rating}</span>
-        );
-    }
+export default class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <span className="rating">{rating}</span>
+    );
+  }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
