186d80690187833a6f992e8cd1e94a1fa2ede2f6
vinicius rampazzo <vinicius_rampazzo10@hotmail.com>
2021-10-06 17:45:36 -0300

    realizando requisito 11

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,9 @@
-// implement Rating component here
+import React, { Component } from 'react';
+
+class Rating extends Component {
+    render() {
+        return <h1>ola, mundo</h1>
+    }
+}
+
+export default Rating;
