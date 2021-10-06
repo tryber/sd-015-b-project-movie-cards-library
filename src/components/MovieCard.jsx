22c0be7aff9e2d1132bdf3258b4bee2456ec9df3
Lucas Rodrigues <theluks21@live.com>
2021-10-06 20:44:18 -0300

    Adiciona requisito 8

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,9 +6,9 @@ class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
-    <div className="movie-card">
-      <img src={imagePath} />
-      
+    <div className="movie-card movie-card-body">
+      <img src={imagePath} className="movie-card-image" alt={title} />
+      <h4 className="movie-card-title">{title}</h4>
     </div>
     )
   }
