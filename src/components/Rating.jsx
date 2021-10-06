b146d64b6536e9cd829ebd19441965f7fcf7be04
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-10-06 16:20:42 -0300

    Requisito 11 completo

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,13 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+// Rating.propTypes = {
+// };
+
+export default Rating;
