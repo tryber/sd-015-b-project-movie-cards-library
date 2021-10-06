f2087d65d316ce2d23b1e2ff736c5537a4f9f7d7
LeonardoMonti <contatoleomonti@gmail.com>
2021-10-06 12:21:27 -0300

    adiciona requisitos 11, 12, 13 e 14

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath , rating } = movie;
 
     return (
       <section>
@@ -11,6 +12,7 @@ class MovieCard extends React.Component {
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
+        <Rating rating={ rating } />
       </section>
     );
   }
