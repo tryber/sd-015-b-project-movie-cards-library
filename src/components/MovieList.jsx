948c37344f7c33ec7718bbe23a086f09aa488e84
henrique <hickm018@gmail.com>
2021-10-06 00:35:06 -0300

    Famoso comitâo bomba faltando 1 requisito

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return 
+    const { movies } = this.props;
+    return (
+      movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+    );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
 
 export default MovieList;
