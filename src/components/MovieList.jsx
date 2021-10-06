0c366123fa1c05ad19349cf659a0911cab2111dd
Andre Luiz <andreluizgyn1@gmail.com>
2021-10-06 19:33:39 -0300

    Corrige para passar no lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-    const allMovies = movies.map((movie) => {
-      return <MovieCard key={ movie.title } />;
-    });
+    const allMovies = movies.map((movie) => <MovieCard key={ movie.title } />);
 
     return allMovies;
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
