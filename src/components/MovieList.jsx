a3f375d7aa7a95215d601cd749473e49f9578cda
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-06 02:38:16 -0300

    Renderiza componentes <MovieCard /> dentro de MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,15 @@
 // implement MovieList component here
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const movies = this.props;
+    const { movies } = this.props;
     return (
-      <main> </main>
+      <main>
+        <h1>arthur</h1>
+        {movies.map((movieObj) => <MovieCard key={ movieObj.title } />)}
+      </main>
     );
   }
 }
