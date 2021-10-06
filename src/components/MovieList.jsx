280481938ad8670a9a834f5900e55bb3e43c2fc7
allpha23 <allpha23@gmail.com>
2021-10-06 16:46:18 -0300

    Requisito 3 finalizado

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const {movies} = this.props 
+    return (
+      <article>
+        <h2>{movies.title}</h2>
+      </article>
+    );
+  }
+}
+
+export default MovieList;
