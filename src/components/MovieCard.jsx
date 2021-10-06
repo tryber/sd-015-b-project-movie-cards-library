0b8df39c819ee16181e205b70126233b39c808bc
Mauricio <mlopeesz@gmail.com>
2021-10-06 17:20:49 -0300

    Adicionado informacoes do filme em cada MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
-      <div>...</div>
+      <div>
+        <img src={ movie.imagePath } alt="Capa do Filme" />
+        <h4>{movie.title}</h4>
+        <h5>{movie.subtitle}</h5>
+        <p>{movie.storyline}</p>
+      </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    imagePath: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
