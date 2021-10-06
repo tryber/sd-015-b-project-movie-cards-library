bf869ce9404fa36f84d9f8efab5a7c3c30c56ad3
Denis Jonathan <denisjonathan7@gmail.com>
2021-10-06 13:27:41 -0300

    Requirement 4: completed

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
+
+    return (
+      <div>
+        {movies.map((movie) => <MovieCard />)}
+      </div>
+    );
   }
 }
 
