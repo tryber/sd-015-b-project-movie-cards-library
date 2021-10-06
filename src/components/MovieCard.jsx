ff98883e1abbdbf029872932a04809aa6bdac743
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-06 18:31:51 -0300

    Refatoração de código

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -19,18 +19,14 @@ class MovieCard extends React.Component {
   }
 }
 
-MovieCard.defaultProps = {
-  movie: PropTypes.object,
-};
-
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string,
-    subtitle: PropTypes.string,
-    storyline: PropTypes.string,
-    imagePath: PropTypes.string,
-    rating: PropTypes.number,
-  }),
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }).isRequired,
 };
 
 export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -11,12 +11,8 @@ class Rating extends React.Component {
   }
 }
 
-Rating.defaultProps = {
-  rating: 0,
-};
-
 Rating.propTypes = {
-  rating: PropTypes.number,
+  rating: PropTypes.number.isRequired,
 };
 
 export default Rating;
