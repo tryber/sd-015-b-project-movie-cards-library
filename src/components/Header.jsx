a1e2c6272884ca57a89c5062d825fe0511284387
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-06 01:47:59 -0300

    Renderiza o componente <Header />

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,10 @@
 // implement Header component here // começando projeto
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return <> </>;
+  }
+}
+
+export default Header;
