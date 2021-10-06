43dbd68bad37270c4b1c9b964aaf0081c868ca9d
douglas <douglasmonteiro.m@gmail.com>
2021-10-06 16:26:59 -0300

    Adiciona requisitos 8, 9 e 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,12 @@ class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     return (
-      <img src={ movie.imagePath } alt={ movie.title } />
+      <div>
+        <img src={ movie.imagePath } alt={ movie.title } />
+        <h4>{movie.title}</h4>
+        <h5>{movie.subtitle}</h5>
+        <p>{movie.storyline}</p>
+      </div>
     );
   }
 }
