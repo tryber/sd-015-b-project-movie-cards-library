ae09708a0bfe4cf1ec6b9a6c782645b2a1563661
Lucas Rodrigues <theluks21@live.com>
2021-10-06 20:32:42 -0300

    Adiciona o requisito 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,5 @@
 // implement MovieCard component here
 import React from 'react';
-// import movies from '../data';
 
 
 class MovieCard extends React.Component {
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,6 @@
 // implement MovieList component here
 import React from 'react';
 import MovieCard from './MovieCard';
-// import Movies from '../data';
 
 class MovieList extends React.Component {
   render() {
