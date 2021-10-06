67f918ec84c6da17f80ba0b64f7610a09861a40f
Yamamoto Andre <andre.yamamoto07@gmail.com>
2021-10-06 19:36:31 -0300

    PropType MovieCards

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 // implement MovieCard component here
+import PropTypes from 'prop-types';
 import React from 'react';
 import Rating from './Rating';
 
@@ -7,12 +8,17 @@ export default class MovieCard extends React.Component {
     const { movie } = this.props;
     return (
       <section>
-        <h4>{movie.title}</h4>
-        <h5>{movie.subtitle}</h5>
-        <img src={movie.imagePath}></img>
-        <p>{movie.storyline}</p>
-        <Rating rating={movie.rating} />
+        <h4>{ movie.title }</h4>
+        <h5>{ movie.subtitle }</h5>
+        <img src={ movie.imagePath }></img>
+        <p>{ movie.storyline }</p>
+        <Rating rating={ movie.rating } />
       </section>
     );
   }
 }
+
+MovieCard.PropTypes = {
+  movie: PropTypes.objectOf(String).isRequired,
+  rating: PropTypes.number.isRequired,
+};
