539a3fb3ddf2b93b293839db60ef039cb655e847
matheusg18 <matheusgabriel.18@gmail.com>
2021-10-06 17:32:09 -0300

    Requisito 13 feito

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,7 @@
 // implement MovieCard component here
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 export default class MovieCard extends React.Component {
   render() {
@@ -12,6 +13,7 @@ export default class MovieCard extends React.Component {
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
+        <Rating />
       </div>
     );
   }
