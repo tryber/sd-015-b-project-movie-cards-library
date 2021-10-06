ce148127a605095b5b16a797d0fe9cbe6918b07a
Caio R. Vieira <caio.robs98@gmail.com>
2021-10-06 15:04:02 -0300

    Component Movie List pronto

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+export default function MovieList({ movies }) {
+  const moviesList = movies;
+  return (
+    <div>
+      {moviesList.map((movie) => <MovieCard movie={ movie } key={ movie.title } />)}
+    </div>
+  );
+}
