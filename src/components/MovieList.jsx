1e8d8baaf82a093123d5751db3cfa660c009d644
iuryroque <iury.roque@hotmail.com>
2021-10-06 18:12:14 -0300

    Projeto Finalizado

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,23 @@
 // implement MovieList component here
-import React from "react";
-// import ReactPropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
-class MovieList extends React.Component{
-    render() {
-        // const { movies } = this.props;
-        return(
-
-        );
-    }
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section className="movie-list">
+        {movies.map((movie) => (
+          <MovieCard key={ movie.title } movie={ movie } />
+        ))}
+      </section>
+    );
+  }
 }
 
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf
+};
 
 export default MovieList;
