a170c5e14829bbf19f2f0a0ddabf8def6f78c1e1
assisMeneghetti <assismfabri@hotmail.com>
2021-10-06 16:50:02 -0300

    finaliza o projeto

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,21 @@
 // implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />);
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    movie: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+    }),
+  })).isRequired,
+};
+
+export default MovieList;
