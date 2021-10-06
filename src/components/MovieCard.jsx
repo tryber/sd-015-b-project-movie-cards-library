fed5bd4ce12590de0cb2043da338c951f4f30074
Wilian Pereira <wilian_pereira92@outlook.com>
2021-10-06 18:10:37 -0300

    Adicionado Requisito 9

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,6 +9,7 @@ class MovieCard extends React.Component {
       <div>
         <img src={ movie.imagePath } alt={ movie.title } />
         <h4>{ movie.title}</h4>
+        <h5>{ movie.subtitle}</h5>
       </div>
     );
   }
