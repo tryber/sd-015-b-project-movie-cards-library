b3f4bffb0064ce2e2b59d9d25a1671096e4ab134
Allan Melo <allan-m-silva@hotmail.com>
2021-10-06 18:18:39 -0300

    ajusta codigo para rodar novamente requisitos 3, 4 e 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <section>
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
@@ -11,5 +12,17 @@ class MovieList extends React.Component {
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
 
 export default MovieList;
