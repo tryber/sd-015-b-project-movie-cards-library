c872f0793c69351cd1cb4546ab3ab2d0336c1a77
Caio R. Vieira <caio.robs98@gmail.com>
2021-10-06 15:54:16 -0300

    redesign movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -11,11 +11,13 @@ import Rating from './Rating';
 function MovieCard({ movie, key }) {
   console.log(key);
   return (
-    <div className="movie-card movie-card-body">
-      <h4 className="movie-card-title">{movie.title}</h4>
-      <h5 className="movie-card-subtitle">{movie.subtitle}</h5>
-      <p className="movie-card-storyline">{movie.storyline}</p>
+    <div className="movie-card">
       <img src={ movie.imagePath } alt={ movie.title } className="movie-card-image" />
+      <div className="movie-card-body">
+        <h4 className="movie-card-title">{movie.title}</h4>
+        <h5 className="movie-card-subtitle">{movie.subtitle}</h5>
+        <p className="movie-card-storyline">{movie.storyline}</p>
+      </div>
       <Rating rating={ movie.rating } />
     </div>
   );
