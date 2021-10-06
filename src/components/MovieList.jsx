65485bec04a7cb3909236987d355c7afc344e328
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-06 01:55:08 -0300

    Renderiza o componente <MovieList />

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const movies = this.props;
+    return (
+      <main> </main>
+    );
+  }
+}
+
+export default MovieList;
