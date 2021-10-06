5ffe2a350ac3be7a1bfa98f39771391680a6265b
Amanda Fernandes <aferanda@gmail.com>
2021-10-06 16:28:44 -0300

    Requisitos 3 a 10

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import movies from './data';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <MovieList movies={ movies } />
       </div>
     );
   }
