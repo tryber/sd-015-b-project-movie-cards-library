953dc1d87c96e87664e0cdf77e3a0f5b001e9118
Ge <gmarcoscardoso@gmail.com>
2021-10-06 17:16:46 -0300

    Implementando css

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,8 +5,9 @@ import MovieCard from './MovieCard';
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
+    console.log(this);
     return (
-      <div>
+      <div className="movie-list">
         {movies.map((movie) => (
           <MovieCard key={ movie.title } movie={ movie } />
         ))}
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -6,9 +6,9 @@ class Rating extends React.Component {
     const { rating } = this.props;
 
     return (
-      <section className="rating">
-        <span>{rating}</span>
-      </section>
+      <div className="movie-card-rating">
+        <span className="rating ">{rating}</span>
+      </div>
     );
   }
 }
