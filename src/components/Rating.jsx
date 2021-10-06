cda98c837f9f75338aee7947f40dfcbd4168fefb
Wilian Pereira <wilian_pereira92@outlook.com>
2021-10-06 18:17:18 -0300

    Adicionado Requisito 11

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,14 @@
 // implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Rating;
