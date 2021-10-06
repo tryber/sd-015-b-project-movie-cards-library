30a77873a4744d94f425f69d2ab2505e0dd61c37
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2021-10-06 19:44:44 -0300

    comita

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 
 class Moviecard extends Component {
   render() {
     const { movie } = this.props;
     return (
       <article>
-        <img src= { movie.imagePath } alt={ movie.title} />
+        <img src={ movie.imagePath } alt={ movie.title } />
         <h4>{ movie.title }</h4>
         <h5>{ movie.subtitle}</h5>
         <p>{ movie.storyline }</p>
@@ -14,16 +14,5 @@ class Moviecard extends Component {
     );
   }
 }
-
-Moviecard.prototype = {
-  movies: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string,
-    subtitle: PropTypes.string,
-  }).isRequired),
-};
-
-Moviecard.defaulProps = {
-  movies: [],
-};
 
 export default Moviecard;
