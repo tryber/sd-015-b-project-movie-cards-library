c2370fa25fa8db9bc7a4a22285a1853a2d6d5ed8
Hugoacr <hugoacr@yahoo.com.br>
2021-10-07 10:19:44 -0300

    corrige componentes para requisitos 4 e 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <div className="movie-list">
         {movies.map((movie) => (<MovieCard
@@ -16,3 +17,7 @@ class MovieList extends React.Component {
 }
 
 export default MovieList;
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
