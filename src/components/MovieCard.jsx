7382c946c1818b92fbbc1569f328b905bccc1958
Rafael <rafael.gomesbarretomoura@gmail.com>
2021-10-06 23:26:50 -0300

    adiciona ultimos requisitos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,7 @@
 // implement MovieCard component here
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -13,6 +14,7 @@ class MovieCard extends React.Component {
           <h4>{movie.title}</h4>
           <h5>{movie.subtitle}</h5>
           <p>{movie.storyline}</p>
+          <Rating rating={ movie.rating } />
         </div>
       </div>
     );
@@ -25,6 +27,7 @@ MovieCard.propTypes = {
     subtitle: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
     imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
   }).isRequired,
 };
 
