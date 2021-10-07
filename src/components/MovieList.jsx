e2bfd2a61588e120a07e6432202e00a2d5d13e08
Joao Pedro de Oli. Lage <JoaoPedro.Lage@outlook.com>
2021-10-06 23:33:11 -0300

    Cria e Renderiza o <MovieList />

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieCard component here
+import React from 'react';
+import movies from '../data';
+// import { v4 as generateId } from 'uuid';
+// import PropTypes  from 'prot-types';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <MovieList movies={ movies } />
+    );
+  }
+}
+
+export default MovieList;
