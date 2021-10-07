6322d941f91f924f8cd90eedbbd5b89fe48e006e
Lucas Peres <lucasperesss@gmail.com>
2021-10-06 21:19:42 -0300

    componente Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -10,10 +11,12 @@ class MovieCard extends React.Component {
         <h4>{movie.title}</h4>
         <h5>{movie.subtitle}</h5>
         <p>{movie.storyline}</p>
+        <Rating rating={ movie.rating } />
       </div>
     );
   }
 }
+
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
