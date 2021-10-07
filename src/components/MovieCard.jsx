193268a0409693fecd609905f0c7e6a90e1244bf
Yamamoto Andre <andre.yamamoto07@gmail.com>
2021-10-06 21:51:12 -0300

    colocando CSS na pagina para ficar bonito

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -11,8 +11,10 @@ export default class MovieCard extends React.Component {
         <img src={ movie.imagePath } className="movie-card-image" alt={ movie.title } />
         <div className="movie-card-body">
           <h4 className="movie-card-title">{ movie.title }</h4>
-          <h5>{ movie.subtitle }</h5>
-          <p>{ movie.storyline }</p>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline">{ movie.storyline }</p>
+        </div>
+        <div className="movie-card-rating">
           <Rating rating={ movie.rating } />
         </div>
       </section>
