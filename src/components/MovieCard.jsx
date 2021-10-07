79cc686cf1a1f6efa7c98305b15e3bc7b3910341
Laurenz Pinder <laurenzdpinder90@gmail.com>
2021-10-06 21:07:36 -0300

    Adiciona classes css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,14 +6,14 @@ export default class MovieCard extends Component {
   render() {
     const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
-      <div>
-        <div>
-          <img src={ imagePath } alt="" />
-          <h4>{ title }</h4>
-          <h5>{ subtitle }</h5>
-          <p>{ storyline }</p>
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <img className="movie-card-image" src={ imagePath } alt="" />
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
         </div>
-        <div>
+        <div className="movie-card-rating">
           <Rating rating={ rating } />
         </div>
       </div>
