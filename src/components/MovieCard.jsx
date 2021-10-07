af31d1e6c5235eddf4d8d2a41bb4cd6fdfb2d674
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-10-06 21:40:40 -0300

    terminado requisito 9

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../App.css';
 
-function MovieCard({ movie: { title, imagePath } }) {
+function MovieCard({ movie: { title, subtitle, imagePath } }) {
   return (
     <section>
       <div>
         <img src={ imagePath } alt={ title } />
         <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
       </div>
     </section>
   );
