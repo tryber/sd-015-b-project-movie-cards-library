34bda0e397c1c57f57a856837c247dc85d12fa6b
alxedv <alexmaia.etel@gmail.com>
2021-10-07 17:25:54 -0300

    Adiciona os requisito 4 e 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,18 @@
 // implement MovieList component here
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return <article></article>;
+const { movies } = this.props
+
+    return (
+      <section>
+          {movies.map((movies) => {
+            return <MovieCard key={movies.title} movie={movies}/>;
+        })}
+      </section>
+    )
   }
 }
 
