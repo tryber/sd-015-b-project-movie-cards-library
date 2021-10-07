8c5d37c329f2720269fde5d3aaa350607053dbbb
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-06 23:04:26 -0300

    Finaliza Componente Rating.

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,2 +1,15 @@
 // implement Rating component here
-// import React from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieRating extends React.Component {
+    render() {
+        const { rating } = this.props;
+      return (
+        <p className="rating"> { rating } </p>
+      );
+    }
+  }
+  
+  export default MovieRating;
+  
