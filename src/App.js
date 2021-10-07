0200ac475cbdb33c9ca5bcecc9583933f14c465c
Gabriel Borges <igblborges@gmail.com>
2021-10-07 10:57:39 -0300

    requisito-4-propTypes-problems-solved

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import movies from './data';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
-        <section className="movie-list">
-          <MovieList />
+        <section className="moviea-list">
+          <MovieList movies={ movies } />
         </section>
       </div>
     );
