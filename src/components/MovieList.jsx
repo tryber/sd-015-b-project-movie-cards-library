c2ef935c0691912e13f0779c6060c7922e39f8a7
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-06 22:10:23 -0300

    Corrige Lint MovieList.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -8,9 +8,9 @@ import movies from '../data';
 class MovieList extends React.Component {
   render() {
     return (
-        <div className="movie-list">
-          { movies.map( (movie) => <MovieCard movie={ movie } key={ movie.title } />) }
-        </div>
+      <div className="movie-list">
+        { movies.map( (movie) => <MovieCard movie={ movie } key={ movie.title } />) }
+      </div>
     );
   }
 }
