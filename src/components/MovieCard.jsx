cdddc700604eacbaf62db731f7f92f07276621be
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-06 22:08:50 -0300

    Adiciona proptypes para MovieCard.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,24 +1,34 @@
 // implement MovieCard component here
 import React from 'react';
+import PropTypes from 'prop-types';
 // import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating} = movie;
     return (
       <div className="movie-card">
         <section className="movie-card-body">
-          <h4 className="movie-card-title">{ movie.title }</h4>
-          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
-          <p className="movie-card-storyline">{ movie.storyline }</p>
-          <img className="movie-card-image" src={ movie.imagePath } alt={ movie.alt } />
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+          <img className="movie-card-image" src={ imagePath } alt={ title } />
         </section>
         <section className="movie-card-rating">
-          { movie.rating }
+          { rating }
         </section>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline: PropTypes.string,
+  imagePath: PropTypes.string,
+  rating: PropTypes.number
+};
 
 export default MovieCard;
