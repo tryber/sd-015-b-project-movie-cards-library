fd515a1e055ce8ea58e9bb6e2fc6fa7c4e436ae0
Thirki <cicero.santos62@etec.sp.gov.br>
2021-10-07 07:52:08 -0300

    fix: redundancia de PropTypes

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -24,10 +24,6 @@ MovieList.propTypes = {
   movies: PropTypes.arrayOf(
     PropTypes.shape({
       title: PropTypes.string,
-      subtitle: PropTypes.string,
-      storyline: PropTypes.string,
-      rating: PropTypes.number,
-      imagePath: PropTypes.string,
     }),
   ).isRequired,
 };
