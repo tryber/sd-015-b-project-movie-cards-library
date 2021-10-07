2ae88d9fc0974a46cd843836ea91e45a800e06b9
samuel <samuelsoutosilva14@gmail.com>
2021-10-07 13:49:56 -0300

    Adiciona Requisito 14

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -12,7 +12,7 @@ class MovieCard extends React.Component {
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
         <p className="movie-card-storyline">{ storyline }</p>
-        <Rating />
+        <Rating rating={ rating } />
       </div>
     );
   }
