b544a60175b8d7a8d20a786bf7f45b29d9f406d0
Laurenz Pinder <laurenzdpinder90@gmail.com>
2021-10-06 21:49:17 -0300

    Corrige erro de lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,7 +8,11 @@ export default class MovieCard extends Component {
     return (
       <div className="movie-card">
         <div className="movie-card-body">
-          <img className="movie-card-image" src={ imagePath } alt={ `${title} ${subtitle}` } />
+          <img
+            className="movie-card-image"
+            src={ imagePath }
+            alt={ `${title} ${subtitle}` }
+          />
           <h4 className="movie-card-title">{ title }</h4>
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
