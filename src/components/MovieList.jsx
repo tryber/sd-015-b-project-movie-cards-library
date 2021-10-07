56d73d370617db4a0c51ee33899810a78c028a9e
alxedv <alexmaia.etel@gmail.com>
2021-10-07 17:33:39 -0300

    Corrige erros do lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,13 +4,11 @@ import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-const { movies } = this.props
+    const { movies } = this.props;
 
     return (
       <section>
-          {movies.map((movies) => {
-            return <MovieCard key={movies.title} movie={movies}/>;
-        })}
+            {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </section>
     )
   }
