b9216fe4004e959f9fab68c7193f15a34b103980
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-06 21:30:54 -0300

    Adiciona HoF ao MovieList.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,2 +1,22 @@
 // implement MovieList component here
-// import React from 'react';
+import React from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
+
+// Função que renderiza os elementos
+function renderMovieCard(movie) {
+
+}
+
+class MovieList extends React.Component {
+    render() {
+      return (
+          <div className="movie-list">
+            { movies.map( (movie) => <MovieCard movie={ movie } key={ movie.title } />) }
+          </div>
+      );
+    }
+  }
+  
+  export default MovieList;
+  
