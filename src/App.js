7dddafa56e2ce14af71b939012148303cb6116ad
Adriana <adrianam754@gmail.com>
2021-10-06 21:59:31 -0300

    Adicionada requisitos do 3 ao 16

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
+import movies from './data';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 
 class App extends React.Component {
   render() {
     return (
-      <main>
+      <div className="App">
         <Header />
-      </main>
+        <MovieList movies={ movies } />
+      </div>
     );
   }
 }
