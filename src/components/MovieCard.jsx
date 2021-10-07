3039536e6a1bac17c6e74e4bd496cae4fbde9b69
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-06 21:29:37 -0300

    Adiciona props ao MovieCard.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,21 +1,17 @@
 // implement MovieCard component here
 import React from 'react';
-// import movies from '../data';
-
-// Função que renderiza os elementos
-// function renderMovieCard() {
-
-// }
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
       <div className="movie-card">
         <section className="movie-card-body">
           <h4 className="movie-card-title">Title</h4>
           <h5 className="movie-card-subtitle">Subtitle</h5>
           <p className="movie-card-storyline">Story Line</p>
-          <img className="movie-card-image" src="" alt="Teste" />
+          <img className="movie-card-image" src="{Link}" alt="Teste" />
         </section>
         <section className="movie-card-rating">
           Nota
