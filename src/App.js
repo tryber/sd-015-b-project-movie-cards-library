a9fdd37ed3807b4fdbd5c53b18bb638ed0bf46b1
Rodolfomno <Rodolfo_mno@hotmail.com>
2021-10-06 21:38:05 -0300

    adiciona requisito 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      {}
-    </div>
+    <Header />
   );
 }
 
