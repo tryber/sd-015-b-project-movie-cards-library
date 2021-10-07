a93913ece2778226342c1382ec7dbff5852b3634
israel jeronimo <silvaij16@gmail.com>
2021-10-07 12:12:38 -0300

    3 - Crie um componente `<MovieList />`

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      Começando o projeto!
+      <MovieList filmes={ movies } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
-// implement MovieList component here
+import React from 'react';
+// import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    // const { movies } = this.props;
+
+    return (
+      <div>
+        <h1>teste</h1>
+        {/* { movies.map((movie) => <MovieCard key=
+          { movies.title } movie={ movie } />)} */}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
