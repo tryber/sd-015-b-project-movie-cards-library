c85f154babec0b1de07c70dd25adcd3780e697d4
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2021-10-07 15:03:09 -0300

    Welckson Valentim - Finaliza projeto movie cards library apos renderizaçao via css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,11 +7,17 @@ class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     return (
-      <div>
-        <h4>{movie.title}</h4>
-        <h5>{movie.subtitle}</h5>
-        <p>{movie.storyline}</p>
-        <img src={ movie.imagePath } alt={ movie.imagePath } />
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <img
+            className="movie-card-image"
+            src={ movie.imagePath }
+            alt={ movie.imagePath }
+          />
+          <h4 className="movie-card-title">{movie.title}</h4>
+          <h5 className="movie-card-subtitle">{movie.subtitle}</h5>
+          <p className="movie-card-storyline">{movie.storyline}</p>
+        </div>
         <Rating rating={ movie.rating } />
       </div>
     );
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -6,7 +6,8 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <div>
+      <div className="movie-card-rating">
+        <h4 className="movie-title-rating">Rating</h4>
         <p className="rating">{rating}</p>
       </div>
     );
