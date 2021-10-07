eca41fe2ec1c4d20093d91a45145740e1bbc2c7e
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-10-06 21:33:03 -0300

    terminado requisito 4

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 import '../App.css';
 
-function MovieList() {
-
+function MovieList({ movies }) {
+  return (
+    <div>
+      {
+        movies.map((movie) => (
+          <MovieCard movie={ movie } />
+        ))
+      }
+    </div>
+  );
 }
 
 export default MovieList;
