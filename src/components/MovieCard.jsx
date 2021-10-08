c926ffa28e8f17622b3a8c60f37dbbb7136337a6
José Erivam <erivamdev@gmail.com>
2021-10-08 20:56:36 -0300

    mudança

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,22 +4,22 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     // criando uma constante para receber a chave do props
-    const { propMovies } = this.props;
+    const { movies } = this.props;
     // console.log(propMovies);
     return (
       <article>
-        <img src={ propMovies.imagePath } alt={ propMovies.title } />
-        <h4>{`Titulo: ${propMovies.title}`}</h4>
-        <h5>{`Subtitulo: ${propMovies.subtitle}`}</h5>
-        <p>{`Sinopse: ${propMovies.storyline}`}</p>
-        <p>{`Rating: ${propMovies.rating}`}</p>
+        <img src={ movies.imagePath } alt={ movies.title } />
+        <h4>{`Titulo: ${movies.title}`}</h4>
+        <h5>{`Subtitulo: ${movies.subtitle}`}</h5>
+        <p>{`Sinopse: ${movies.storyline}`}</p>
+        <p>{`Rating: ${movies.rating}`}</p>
       </article>
     );
   }
 }
 
 MovieCard.propTypes = {
-  propMovies: PropTypes.arrayOf(PropTypes.shape({
+  movies: PropTypes.arrayOf(PropTypes.shape({
     imagePath: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string.isRequired,
@@ -28,7 +28,7 @@ MovieCard.propTypes = {
   })),
 };
 MovieCard.defaultProps = {
-  propMovies: PropTypes.arrayOf(PropTypes.shape({
+  movies: PropTypes.arrayOf(PropTypes.shape({
     imagePath: '',
     title: 'Sem conteudo',
     subtitle: 'sem conteudo',
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { v4 } from 'uuid';
-import movies from '../data';
+// import movies from '../data';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
@@ -10,16 +10,25 @@ class MovieList extends React.Component {
       elMovie.id = v4();
       return elMovie;
     }); */
-
     // console.log(moviesWithId);
+    const { movies } = this.props;
     return (
       <section>
         {movies
           .map((elementMovies) => (
-            <MovieCard key={ elementMovies.title } propMovies={ elementMovies } />
+            <MovieCard key={ elementMovies.title } movies={ elementMovies } />
           ))}
       </section>
     );
   }
 }
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    imagePath: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  })).isRequired,
+};
 export default MovieList;
