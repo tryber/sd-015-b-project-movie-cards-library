7993c4612a030da36ed35d49a6b3bdf931cc535c
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-10-08 18:55:46 -0300

    [Requisito 9] - Renderiza o subtitulo do filme dentro de um h5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 class MovieCard extends React.Component {
   render() {
     return(
-      <div className='movie-card'>
-        <img src={this.props.movie.imagePath} alt={this.props.movie.title} />
-        <h4>{this.props.movie.title}</h4>
+      <div className="movie-card">
+        <img src={ this.props.movie.imagePath } alt={ this.props.movie.title } />
+        <h4>{ this.props.movie.title }</h4>
+        <h5>{ this.props.movie.subtitle }</h5>
       </div>
     );
   }
