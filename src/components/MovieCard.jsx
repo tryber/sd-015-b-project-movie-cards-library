b53e9bac9ca66d4d39bf0115a05aa16bd66359c6
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-10-08 18:56:24 -0300

    [Requisito 10] - Renderiza a sinopse do filme dentro de uma tag p

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,6 +7,7 @@ class MovieCard extends React.Component {
         <img src={ this.props.movie.imagePath } alt={ this.props.movie.title } />
         <h4>{ this.props.movie.title }</h4>
         <h5>{ this.props.movie.subtitle }</h5>
+        <p>{ this.props.movie.storyline }</p>
       </div>
     );
   }
