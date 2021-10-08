0673713ede7003dda1ce38a19d55145692dadf39
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2021-10-08 18:48:38 -0300

    4th to 10th requisites

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return <article> </article>;
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
+    );
   }
 }
 
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+};
+
+MovieList.defaultProps = {
+  movies: [{}],
+};
 export default MovieList;
