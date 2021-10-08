ac6073ae3aba1b08fe35e1f141fbe2c7c69739aa
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-10-08 18:53:07 -0300

    [Requisito 6] - Cria o componente MovieCard

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return(
+      <div>
+        <span>Renderizado</span>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
