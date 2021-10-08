747a41834f538ee7a92ffe92bc0f6862e96174bb
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-10-08 17:58:04 -0300

    [Requisito 15] - Cria o componente App e renderiza o componente Header dentro dele

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import Header from './components/Header';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
