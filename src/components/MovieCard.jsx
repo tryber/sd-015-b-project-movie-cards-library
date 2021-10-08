6c37b929a07a8505f0dd7eaf1c9a9781504d0149
fariasao <enzofarias2010@gmail.com>
2021-10-07 23:03:12 -0300

    requisitos 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,6 +9,7 @@ class MovieCard extends React.Component {
       <div>
         <h4>{ movie.title }</h4>
         <h5>{ movie.subtitle }</h5>
+        <p>{ movie.storyline }</p>
         <img src={ movie.imagePath } alt={ movie.title } />
       </div>
     );
