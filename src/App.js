839b38bbbc9115b2b89a027fd21e0cf23c4b372b
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-10-08 18:20:04 -0300

    [Reset]

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Header from './components/Header';
-import MovieList from './components/MovieList';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
-      <MovieList />
+    <div className="App">
+
     </div>
   );
 }
