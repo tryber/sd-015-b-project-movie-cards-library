8594b584302ecc7f6ee1987cf226a6c41a0a6775
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-10-08 17:37:31 -0300

    [Requisito 8] - Renderiza o titulo do filme dentro de um h4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,6 +4,7 @@ class MovieCard extends React.Component {
   render() {
     return (
       <div className='movie-card'>
+        <h4>{this.props.movie.title}</h4>
         <img src={this.props.movie.imagePath} alt={this.props.movie.title} />
       </div>
     );
