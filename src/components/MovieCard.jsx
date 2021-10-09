48101fc9d5ffc276e838e8f7d3d6b49850d13a58
CaioMagalhaesPinheiro <caiomgp22@gmail.com>
2021-10-09 20:48:47 -0300

    Adicionando classname

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,11 +7,11 @@ class MovieCard extends React.Component {
     const { movie } = this.props;
     const { title, subtitle, storyline, rating, imagePath } = movie;
     return (
-      <section>
-        <img src={ imagePath } alt={ title } />
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
+      <section className='movie-card movie-card-body'>
+        <img src={ imagePath } alt={ title } className='movie-card-image'/>
+        <h4 className='movie-card-title'>{ title } </h4>
+        <h5 className='movie-card-subtitle'>{ subtitle }</h5>
+        <p className='movie-card-storyline'>{ storyline }</p>
         <Rating rating={ rating } />
       </section>
     );
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,7 +6,7 @@ class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <article>
+      <article className='movie-list'>
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </article>
     );
