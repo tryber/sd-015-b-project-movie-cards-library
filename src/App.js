264b3d78a706f4dd9417e829c3f2f1da1a52036e
Rodolfomno <Rodolfo_mno@hotmail.com>
2021-10-09 21:18:50 -0300

    finaliza projeto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import MovieList from './components/MovieList';
+import movies from './data';
 
 function App() {
   return (
-    <Header />
+    <main>
+      <Header />
+      <MovieList movies={ movies } />
+    </main>
   );
 }
 
