12af0285ecb54e53f3509eba5833db3bfdfef872
Toledodev <rafael.toledo@engenharia.ufjf.br>
2021-10-11 19:35:39 -0300

    Finalizando o projeto com o erro de lint

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Headers from './components/Header';
 import MovieList from './components/MovieList';
+import movies from './data';
 
 function App() {
   return (
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,29 @@
 // implement MovieList component here
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
-class MovieList extends React.Component {
+class MovieList extends Component {
   render() {
+    const { movies } = this.props;
     return (
-      <MovieCard />
+      <div className="movie-list">
+        {movies.map((movie) => (
+          <MovieCard key={ movie.title } movie={ movie } />
+        ))}
+      </div>
     );
   }
 }
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
 export default MovieList;
