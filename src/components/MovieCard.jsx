3eb3ebab0a9c28eba6ffc9b78ededead150616e2
Flávia Labanca <flavialabanca@gmail.com>
2021-10-11 00:46:54 -0300

    requisito

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
-      <p>
-        MovieCard
-      </p>
+      <div>
+        <p>{movie.title}</p>
+        <p>{movie.subtitle}</p>
+        <p>{movie.storyline}</p>
+        <p>{movie.rating}</p>
+      </div>
     );
   }
 }
