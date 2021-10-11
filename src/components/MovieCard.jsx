3075b3c821e6e1dee5c226848d3cddb2321be2a1
Flávia Labanca <flavialabanca@gmail.com>
2021-10-11 01:10:57 -0300

    requisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -10,7 +10,7 @@ class MovieCard extends React.Component {
         <h4>{movie.title}</h4>
         <h5>{movie.subtitle}</h5>
         <p>{movie.storyline}</p>
-        <p>{movie.rating}</p>
+        {/* <Rating rating={ movie.rating } /> */}
         <img src={ movie.imagePath } alt={ movie.title } />
       </div>
     );
