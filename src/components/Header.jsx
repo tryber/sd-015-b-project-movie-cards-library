4c07431bbd4ed3d5500f4dcaa4f59528a8c05bd8
dfernan6 <danilo.afernandes@outlook.com>
2021-10-12 20:00:07 -0300

    Enviado o requisito   background-color: #343a40;

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-// implement Header component here
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+          <header>
+          <h1>Movie Card Library</h1>
+          </header>
+          );
+    }
+}
+
+export default Header
