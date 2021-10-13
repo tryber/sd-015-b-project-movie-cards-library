19a64f0b239790b7a7599d68f251cfcef882b092
alinemsiqueira <aline.mscruz1@gmail.com>
2021-10-13 03:32:12 -0300

    Requirement 01 and 02 - fix lint issues

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header'
+import Header from './components/Header';
 
 function App() {
   return (
-  <div className="App">
+    <div className="App">
       <Header />
-  </div>
-  )
+    </div>
+  );
 }
-
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 // implement Header component here
 import React from 'react';
+
 class Header extends React.Component {
   render() {
     return (
