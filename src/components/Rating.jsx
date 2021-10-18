cd01526752af7775dc3a0c05c8ce95167248e2c6
Vitor Emerich <vitoremerich.contato@gmail.com>
2021-10-18 14:53:50 -0300

    Termina projeto

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,17 @@
-// implement Rating component here
+import React from "react";
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+    render() {
+        const { rating } = this.props;
+        return (
+            <h5 className="rating">{rating}</h5>
+        )
+    }
+}
+
+Rating.propTypes = {
+    rating: PropTypes.number.isRequired,
+};
+
+export default Rating;
