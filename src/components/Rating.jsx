e7f1222421032b81b92a3225820d24ba7dddae75
Vitor Emerich <vitoremerich.contato@gmail.com>
2021-10-19 12:55:27 -0300

    Arruma erro lint

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
-    render() {
-        const { rating } = this.props;
-        return (
-            <h5 className="rating">{rating}</h5>
-        )
-    }
+  render() {
+    const { rating } = this.props;
+    return (
+      <h5 className="rating">{rating}</h5>
+    );
+  }
 }
 
 Rating.propTypes = {
-    rating: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
 };
 
 export default Rating;
