4070f4e3b52c010b119c83de4218035b2fedf944
Jamez Mayke <jamezmayke@gmail.com>
2021-10-22 11:30:15 -0300

    Requisitos Feitos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,12 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <h2>Movie</h2>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
-function MovieList(movies) {
-  return <h1>oi</h1>
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      movies.map((movie) => (
+        <MovieCard key={ movie.title } movie={ movie } />
+      ))
+    );
+  }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
