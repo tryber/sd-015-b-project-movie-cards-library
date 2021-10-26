e777a1540e7dbb5329d444d2c4038745ec42b5b5
Ismael_Luviseti <ismaelluviseti@gmail.com>
2021-10-26 05:11:28 -0300

    requisito 13

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
     const { movie } = this.props;
-    const { title, imagePath, subtitle, storyline } = movie;
+    const { title, imagePath, subtitle, storyline, rating } = movie;
     return (
       <div>
         <img src={ imagePath } alt={ title } />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
+        <div><Rating key={ rating } /></div>
       </div>
     );
   }
@@ -22,7 +24,7 @@ MovieCard.propTypes = {
     imagePath: PropTypes.string.isRequired,
     subtitle: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
-    // rating: PropTypes.number.isRequired,
+    rating: PropTypes.number.isRequired,
   }).isRequired,
 };
 
