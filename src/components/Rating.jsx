d53b6c96e339c935bb84c9a71bfa1db6e3e214c1
Ismael_Luviseti <ismaelluviseti@gmail.com>
2021-10-26 04:37:27 -0300

    requisito 11

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class Rating extends Component {
   render() {
     const { rating } = this.props;
-    
+    return (
+      <div>{ rating }</div>
+    );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
+
+export default Rating;
