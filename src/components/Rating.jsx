859da2acac010b09964e8a3ef58d5750be599f05
Ismael_Luviseti <ismaelluviseti@gmail.com>
2021-10-26 05:01:53 -0300

    erros de lint ate o 12

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,7 +5,7 @@ class Rating extends Component {
   render() {
     const { rating } = this.props;
     return (
-      <div className='rating'>
+      <div className="rating">
         {rating}
       </div>
     );
