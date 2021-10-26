82cb862d82e727a1fae9079b948c61976b525f26
Ismael_Luviseti <ismaelluviseti@gmail.com>
2021-10-26 04:04:57 -0300

    requisito 9 finalizado

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,11 +4,12 @@ import PropTypes from 'prop-types';
 class MovieCard extends Component {
   render() {
     const { movie } = this.props;
-    const { title, imagePath } = movie;
+    const { title, imagePath, subtitle } = movie;
     return (
       <div>
         <img src={ imagePath } alt={ title } />
         <h4>{title}</h4>
+        <h5>{subtitle}</h5>
       </div>
     );
   }
