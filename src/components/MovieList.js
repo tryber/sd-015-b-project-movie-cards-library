782fab38730ec8fe9d93820b51d9f5ca06ef953a
Ismael_Luviseti <ismaelluviseti@gmail.com>
2021-10-26 03:14:05 -0300

    requisito 1 e 2 finalizado

---
+++ src/components/MovieList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      
+    );
+  }
+}
+
+export default MovieList;
